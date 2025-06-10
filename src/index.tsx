c01a1227ae103a09f85c68bab13b89c2cb4275eb
Richard Wagner <richard.wagner.sd@gmail.com>
2025-06-10 11:10:06 +0200

    fix: initial

---
+++ src/index.tsx
@@ -0,0 +1,8 @@
+import { createRoot } from 'react-dom/client';
+import { RouterProvider } from 'react-router';
+
+import router from './routes/router';
+
+createRoot(document.getElementById('root')!).render(
+  <RouterProvider router={router} />
+);
