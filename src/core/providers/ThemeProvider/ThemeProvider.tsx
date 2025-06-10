6d95a55196aa9d5e876965b206b70d985f7ac954
Richard Wagner <richard.wagner.sd@gmail.com>
2025-06-10 14:03:48 +0200

    fix: provider

---
+++ src/core/providers/ThemeProvider/ThemeProvider.tsx
@@ -0,0 +1,21 @@
+import { type ReactNode, type FC, useState, useMemo } from 'react';
+import { ThemeContext } from '../../contexts/theme/theme';
+import type { Theme } from '../../types';
+
+interface Props {
+  children?: ReactNode;
+}
+
+export const ThemeProvider: FC<Props> = ({ children }) => {
+  const [theme, setTheme] = useState<Theme>('light');
+
+  const ctx = useMemo(
+    () => ({
+      theme,
+      setTheme
+    }),
+    [theme]
+  );
+
+  return <ThemeContext value={ctx}>{children}</ThemeContext>;
+};
