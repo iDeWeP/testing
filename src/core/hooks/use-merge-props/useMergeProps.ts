fa61a8c9bf03a7b29c383dce1e9f7911ec30ffa3
Richard Wagner <richard.wagner.sd@gmail.com>
2025-06-10 14:39:41 +0200

    fix: merge-props

---
+++ src/core/hooks/use-merge-props/useMergeProps.ts
@@ -0,0 +1,19 @@
+import { useContext } from 'react';
+import { ConfigContext } from '../../contexts/config/config';
+import type { CoreComponentsConfig } from '../../contexts/config/config.types';
+
+// CHECL TYPES
+export const useMergeProps = <D, P extends { className: string }>(
+  component: keyof CoreComponentsConfig,
+  defaultProps: D,
+  props: P
+) => {
+  const config = useContext(ConfigContext).core?.[component];
+
+  return {
+    ...defaultProps,
+    ...config?.props,
+    ...props,
+    className: `${config?.styles} ${props.className}`
+  };
+};
