3812f9364d1f5e2cbef7d5e115aac14a921c3682
Richard Wagner <richard.wagner.sd@gmail.com>
2025-06-10 14:06:37 +0200

    fix: use-theme

--- src/core/hooks/use-theme/useTheme.ts
+++ src/core/hooks/use-theme/useTheme.ts
@@ -1,5 +1,4 @@
 import { useContext } from 'react';
 import { ThemeContext } from '../../contexts/theme/theme';
-import type { Theme } from '../../types';
 
-export const useTheme = (): Theme => useContext(ThemeContext).theme;
+export const useTheme = () => useContext(ThemeContext).theme;
