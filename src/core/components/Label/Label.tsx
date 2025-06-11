8730212ef7c261787ea0111d40afa37689afb45c
Richard Wagner <richard.wagner.sd@gmail.com>
2025-06-11 22:33:06 +0200

    fix: label

--- src/core/components/Label/Label.tsx
+++ src/core/components/Label/Label.tsx
@@ -4,7 +4,7 @@ import { Flex } from '../Flex/Flex';
 import type { FlexProps } from '../Flex/Flex.types';
 import labelConfig from './labelConfig';
 
-export const Label = <E extends ElementType = 'div'>(props: FlexProps<E>) => {
+export const Label = <E extends ElementType = 'label'>(props: FlexProps<E>) => {
   const mergedProps = useMergeProps('label', labelConfig.props, props);
 
   return <Flex {...mergedProps} />;
