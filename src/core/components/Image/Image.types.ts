b9f8d0ab5704eb2fc585a8456a20144b55c7b358
Richard Wagner <richard.wagner.sd@gmail.com>
2025-06-11 08:53:28 +0200

    fix: image

--- src/core/components/Image/Image.types.ts
+++ src/core/components/Image/Image.types.ts
@@ -2,9 +2,7 @@ import type { ElementType } from 'react';
 import type { Radius, RefProps } from '../../types';
 
 export type ImageConfigProps = {
-  radius: Radius;
+  radius?: Radius;
 };
 
-export type ImageDefaultProps = Partial<ImageConfigProps>;
-
-export type ImageProps<E extends ElementType> = RefProps<E, ImageDefaultProps>;
+export type ImageProps<E extends ElementType> = RefProps<E, ImageConfigProps>;
