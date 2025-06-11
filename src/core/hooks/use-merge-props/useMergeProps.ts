34795156065cf3625bc3ff46d0bd91244e249075
Richard Wagner <richard.wagner.sd@gmail.com>
2025-06-11 11:49:26 +0200

    fix: mere-props

--- src/core/hooks/use-merge-props/useMergeProps.ts
+++ src/core/hooks/use-merge-props/useMergeProps.ts
@@ -1,13 +1,12 @@
-import { useContext } from 'react';
+import { type ElementType, useContext } from 'react';
 import { ConfigContext } from '../../contexts/config/config';
 import type { CoreComponentsConfig } from '../../contexts/config/config.types';
 
-// CHECL TYPES
 export const useMergeProps = <D extends object, P extends object>(
   component: keyof CoreComponentsConfig,
   defaultProps: D,
   props: P
-) => {
+): Omit<D & P, 'as'> & { as?: ElementType } => {
   const config = useContext(ConfigContext).core?.[component];
 
   return {
