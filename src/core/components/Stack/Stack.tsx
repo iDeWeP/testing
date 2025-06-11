33ba3403feb0318f170f9395920afeecf9375b34
Richard Wagner <richard.wagner.sd@gmail.com>
2025-06-11 22:33:26 +0200

    fix: stack

--- src/core/components/Stack/Stack.tsx
+++ src/core/components/Stack/Stack.tsx
@@ -1,9 +1,10 @@
 import type { ElementType } from 'react';
 import { useMergeProps } from '../../hooks/use-merge-props/useMergeProps';
+import { Flex } from '../Flex/Flex';
 import type { FlexProps } from '../Flex/Flex.types';
 import stackConfig from './stackConfig';
 
-export const Flex = <E extends ElementType = 'div'>(props: FlexProps<E>) => {
+export const Stack = <E extends ElementType = 'div'>(props: FlexProps<E>) => {
   const mergedProps = useMergeProps('stack', stackConfig.props, props);
 
   return <Flex {...mergedProps} />;
