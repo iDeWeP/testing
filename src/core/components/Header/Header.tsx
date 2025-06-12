eb3fe26a0ed531bc883c0ab694123d4f21700489
Richard Wagner <richard.wagner.sd@gmail.com>
2025-06-12 12:35:42 +0200

    fix: import

--- src/core/components/Header/Header.tsx
+++ src/core/components/Header/Header.tsx
@@ -4,7 +4,9 @@ import { Flex } from '../Flex/Flex';
 import type { FlexProps } from '../Flex/Flex.types';
 import headerConfig from './headerConfig';
 
-export const Header = <E extends ElementType = 'header'>(props: FlexProps<E>) => {
+export const Header = <E extends ElementType = 'header'>(
+  props: FlexProps<E>
+) => {
   const mergedProps = useMergeProps('header', headerConfig.props, props);
 
   return <Flex {...mergedProps} />;
