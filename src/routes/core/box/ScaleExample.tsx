3bc99ad1064499d145459b9d7c93da37ec34368d
Richard Wagner <richard.wagner.sd@gmail.com>
2025-06-12 11:48:07 +0200

    fix: scale

--- src/routes/core/box/ScaleExample.tsx
+++ src/routes/core/box/ScaleExample.tsx
@@ -5,12 +5,42 @@ import { Box } from '../../../core/components/Box/Box';
 export const ScaleExample = () => (
   <ExampleSection title="Scale">
     <ExampleCard>
-      <Box scale="square">SQUARE</Box>
-      <Box scale="square-fit">SQUARE-FIT</Box>
-      <Box scale="square-full">SQUARE-FULL</Box>
-      <Box scale="rect">RECT</Box>
-      <Box scale="rect-fit">RECT-FIT</Box>
-      <Box scale="rect-full">RECT-FULL</Box>
+      <Box
+        spacing="md"
+        scale="square"
+      >
+        SQUARE
+      </Box>
+      <Box
+        spacing="md"
+        scale="square-fit"
+      >
+        SQUARE-FIT
+      </Box>
+      <Box
+        spacing="md"
+        scale="square-full"
+      >
+        SQUARE-FULL
+      </Box>
+      <Box
+        spacing="md"
+        scale="rect"
+      >
+        RECT
+      </Box>
+      <Box
+        spacing="md"
+        scale="rect-fit"
+      >
+        RECT-FIT
+      </Box>
+      <Box
+        spacing="md"
+        scale="rect-full"
+      >
+        RECT-FULL
+      </Box>
     </ExampleCard>
   </ExampleSection>
 );
