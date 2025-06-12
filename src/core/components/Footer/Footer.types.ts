a11cd621f9f1a67453712ce6be993453721dacb4
Richard Wagner <richard.wagner.sd@gmail.com>
2025-06-12 12:07:15 +0200

    fix:header

--- src/core/components/Footer/Footer.types.ts
+++ src/core/components/Footer/Footer.types.ts
@@ -1,7 +1,6 @@
 import type { ElementType } from 'react';
-import type { BoxProps } from '../Box/Box.types';
-import type { FlexConfigProps } from '../Flex/Flex.types';
+import type { FlexConfigProps, FlexProps } from '../Flex/Flex.types';
 
 export type FooterConfigProps = FlexConfigProps;
 
-export type FooterProps<E extends ElementType> = BoxProps<E> & FooterConfigProps;
+export type FooterProps<E extends ElementType> = FlexProps<E>;
--- src/core/components/Header/Header.types.ts
+++ src/core/components/Header/Header.types.ts
@@ -1,7 +1,6 @@
 import type { ElementType } from 'react';
-import type { BoxProps } from '../Box/Box.types';
-import type { FlexConfigProps } from '../Flex/Flex.types';
+import type { FlexConfigProps, FlexProps } from '../Flex/Flex.types';
 
 export type HeaderConfigProps = FlexConfigProps;
 
-export type HeaderProps<E extends ElementType> = BoxProps<E> & HeaderConfigProps;
+export type HeaderProps<E extends ElementType> = FlexProps<E>;
