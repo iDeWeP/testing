f5cc34418ad521166ca3099f0caa39fc398d6b52
Richard Wagner <richard.wagner.sd@gmail.com>
2025-06-12 12:27:04 +0200

    fix: card

--- src/core/components/Card/Card.types.ts
+++ src/core/components/Card/Card.types.ts
@@ -1,7 +1,6 @@
 import type { ElementType } from 'react';
-import type { BoxProps } from '../Box/Box.types';
-import type { FlexConfigProps } from '../Flex/Flex.types';
+import type { FlexConfigProps, FlexProps } from '../Flex/Flex.types';
 
 export type CardConfigProps = FlexConfigProps;
 
-export type CardProps<E extends ElementType> = BoxProps<E> & CardConfigProps;
+export type CardProps<E extends ElementType> = FlexProps<E>;
