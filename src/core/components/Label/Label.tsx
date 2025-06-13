cf098373086a0c59f4b6b28efae9ae99dd76f903
Richard Wagner <richard.wagner.sd@gmail.com>
2025-06-13 09:25:37 +0200

    fix: label

--- src/core/components/Label/Label.tsx
+++ src/core/components/Label/Label.tsx
@@ -1,23 +1,13 @@
 import type { ElementType } from 'react';
 import { useMergeProps } from '../../hooks/use-merge-props/useMergeProps';
-import { mergeClassName } from '../../utils/mergeClassName/mergeClassName';
 import { UnstyledTypography } from '../UnstyledTypography/UnstyledTypography';
 import type { LabelProps } from './Label.types';
 import { labelConfig } from './labelConfig';
 
-export const Label = <E extends ElementType = 'label'>(props: LabelProps<E>) => {
-  const { className, ...restProps } = useMergeProps(
-    'label',
-    labelConfig.props,
-    props
-  );
+export const Label = <E extends ElementType = 'label'>(
+  props: LabelProps<E>
+) => {
+  const mergedProps = useMergeProps('label', labelConfig.props, props);
 
-  const mergedClassName = mergeClassName('label', className);
-
-  return (
-    <UnstyledTypography
-      className={mergedClassName}
-      {...restProps}
-    />
-  );
+  return <UnstyledTypography {...mergedProps} />;
 };
--- src/core/components/Mark/Mark.tsx
+++ src/core/components/Mark/Mark.tsx
@@ -1,11 +1,23 @@
 import type { ElementType } from 'react';
 import { useMergeProps } from '../../hooks/use-merge-props/useMergeProps';
+import { mergeClassName } from '../../utils/mergeClassName/mergeClassName';
 import { UnstyledTypography } from '../UnstyledTypography/UnstyledTypography';
 import type { MarkProps } from './Mark.types';
 import { markConfig } from './markConfig';
 
 export const Mark = <E extends ElementType = 'span'>(props: MarkProps<E>) => {
-  const mergedProps = useMergeProps('mark', markConfig.props, props);
+  const { className, ...restProps } = useMergeProps(
+    'mark',
+    markConfig.props,
+    props
+  );
 
-  return <UnstyledTypography {...mergedProps} />;
+  const mergedClassName = mergeClassName('mark', className);
+
+  return (
+    <UnstyledTypography
+      className={mergedClassName}
+      {...restProps}
+    />
+  );
 };
