7d42a15d369790c6f117681ebcf6d1a3839df1ad
Richard Wagner <richard.wagner.sd@gmail.com>
2025-06-16 21:47:57 +0200

    fix: margin

--- src/routes/core/unstyledBox/MarginExample.tsx
+++ src/routes/core/unstyledBox/MarginExample.tsx
@@ -5,13 +5,55 @@ import { UnstyledBox } from '../../../core/components/UnstyledBox/UnstyledBox';
 export const MarginExample = () => (
   <ExampleSection title="Margin">
     <ExampleCard align>
-      <UnstyledBox margin="none">NONE</UnstyledBox>
-      <UnstyledBox margin="xs">XS</UnstyledBox>
-      <UnstyledBox margin="sm">SM</UnstyledBox>
-      <UnstyledBox margin="md">MD</UnstyledBox>
-      <UnstyledBox margin="lg">LG</UnstyledBox>
-      <UnstyledBox margin="xl">XL</UnstyledBox>
-      <UnstyledBox margin="xxl">XXL</UnstyledBox>
+      <UnstyledBox
+        variant="solid"
+        margin="none"
+        color="primary"
+      >
+        NONE
+      </UnstyledBox>
+      <UnstyledBox
+        variant="solid"
+        margin="xs"
+        color="primary"
+      >
+        XS
+      </UnstyledBox>
+      <UnstyledBox
+        variant="solid"
+        margin="sm"
+        color="primary"
+      >
+        SM
+      </UnstyledBox>
+      <UnstyledBox
+        variant="solid"
+        margin="md"
+        color="primary"
+      >
+        MD
+      </UnstyledBox>
+      <UnstyledBox
+        variant="solid"
+        margin="lg"
+        color="primary"
+      >
+        LG
+      </UnstyledBox>
+      <UnstyledBox
+        variant="solid"
+        margin="xl"
+        color="primary"
+      >
+        XL
+      </UnstyledBox>
+      <UnstyledBox
+        variant="solid"
+        margin="xxl"
+        color="primary"
+      >
+        XXL
+      </UnstyledBox>
     </ExampleCard>
   </ExampleSection>
 );
