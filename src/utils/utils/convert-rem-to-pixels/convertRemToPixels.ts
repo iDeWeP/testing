e4c1129b1116070a86591c22b3bef2c67f2e0082
Richard Wagner <richard.wagner.sd@gmail.com>
2025-06-20 22:58:04 +0200

    fix: rename

--- src/utils/utils/convert-rem-to-pixels/convertRemToPixels.ts
+++
@@ -1,7 +0,0 @@
-export const convertRemToPixels = (rem: number) => {
-  const fontSize = getComputedStyle(document.documentElement).fontSize;
-  const rootFontSize = parseFloat(fontSize);
-  const size = isNaN(rootFontSize) ? 16 : rootFontSize;
-
-  return rem * size;
-};
