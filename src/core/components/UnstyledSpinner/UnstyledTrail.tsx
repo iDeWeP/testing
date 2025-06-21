b18b7c0b06ab9b47fdec8c6616a8539539c09b14
Richard Wagner <richard.wagner.sd@gmail.com>
2025-06-22 00:33:36 +0200

    fix; trail

--- src/core/components/UnstyledSpinner/UnstyledTrail.tsx
+++ src/core/components/UnstyledSpinner/UnstyledTrail.tsx
@@ -1,5 +1,5 @@
 import type { MergeComponentProps, Variant, Color } from '../../types';
-import { mergeClassName } from '../../utils/mergeClassName/mergeClassName';
+import { mergeClassName } from '../../utils/mergeClassName/merge-class-name';
 
 type Props = MergeComponentProps<
   'circle',
