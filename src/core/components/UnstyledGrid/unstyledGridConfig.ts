7fb2543f255271798cb063fae2cf200e5ee928e8
Richard Wagner <richard.wagner.sd@gmail.com>
2025-06-22 14:42:35 +0200

    fix: rename

---
+++ src/core/components/UnstyledGrid/unstyledGridConfig.ts
@@ -0,0 +1,23 @@
+import type { ComponentConfig } from '../../types';
+import type { UnstyledGridConfigProps } from './UnstyledGrid.types';
+
+export const unstyledGridConfig: ComponentConfig<UnstyledGridConfigProps> = {
+  props: {
+    as: 'div',
+    variant: 'none',
+    size: 'none',
+    scale: 'normal',
+    margin: 'none',
+    border: false,
+    radius: 'none',
+    color: 'primary',
+    flow: 'row',
+    justifyContent: 'normal',
+    alignContent: 'normal',
+    justifyItems: 'stretch',
+    alignItems: 'stretch',
+    gap: 'none',
+    colGap: 'none',
+    rowGap: 'none'
+  }
+};
