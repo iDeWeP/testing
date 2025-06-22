5d94bf8099340ef5caa3c62fc6bf881b92cabd50
Richard Wagner <richard.wagner.sd@gmail.com>
2025-06-22 14:18:33 +0200

    fix: port-node

--- src/utils/utils/port-node/port-node.ts
+++ src/utils/utils/port-node/port-node.ts
@@ -1,7 +1,7 @@
 import type { ReactNode, Key } from 'react';
 import { createPortal } from 'react-dom';
 
-export const portElement = (
+export const portNode = (
   el: ReactNode,
   destinationEl?: Element | DocumentFragment | null,
   key?: Key | null
