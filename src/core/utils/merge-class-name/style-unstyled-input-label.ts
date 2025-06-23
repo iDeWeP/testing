32631aed7457b725ff281474f25aadebe910ea93
Richard Wagner <richard.wagner.sd@gmail.com>
2025-06-23 17:45:59 +0200

    fix: label

--- src/core/utils/merge-class-name/style-unstyled-input-label.ts
+++ src/core/utils/merge-class-name/style-unstyled-input-label.ts
@@ -1,14 +1,16 @@
 import { unstyledInputConfig } from '../../components/UnstyledInput/unstyledInputConfig';
+import { generic } from '../../config/generic';
 import type { Classes } from '../../types';
 import { getInputVariant } from './get-input-variant';
 import { mergeClasses } from './merge-classes';
 
 export const styleUnstyledInputLabel = (
   className: string,
-  { inputVariant = 'default', inputSize = 'md' }: Classes
+  { inputVariant = 'default', inputSize = 'md', labeled = false }: Classes
 ) =>
   mergeClasses(
     unstyledInputConfig.styles.label.default,
+    labeled && generic.styles.position.relative,
     unstyledInputConfig.styles.label.variant[
       getInputVariant(inputVariant).variant
     ][inputSize],
