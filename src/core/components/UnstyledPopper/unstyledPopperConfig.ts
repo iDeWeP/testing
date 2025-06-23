33e6cb943c6f88942d0e12a096a9bdb94d3491eb
Richard Wagner <richard.wagner.sd@gmail.com>
2025-06-23 17:26:29 +0200

    fix: popper

--- src/core/components/UnstyledPopper/unstyledPopperConfig.ts
+++ src/core/components/UnstyledPopper/unstyledPopperConfig.ts
@@ -19,7 +19,7 @@ export const unstyledPopperConfig: ComponentConfig<UnstyledPopperConfigProps> &
   props: {
     as: 'div',
     defaultOpen: false,
-    collision: 'auto',
+    collision: 'none',
     trigger: 'click',
     followCursor: false,
     closeOnOutClick: false,
@@ -57,7 +57,8 @@ export const unstyledPopperConfig: ComponentConfig<UnstyledPopperConfigProps> &
     root: {
       default: {
         position: 'absolute',
-        transition: 'transition-opacity'
+        transition: 'transition-opacity',
+        transitionTimingFunction: 'easy-in-out'
       }
     },
     backdrop: {
