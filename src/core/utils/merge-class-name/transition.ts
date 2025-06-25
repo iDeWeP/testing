94a3cb2318925920812466cb994f5eb58b2c5bda
Richard Wagner <richard.wagner.sd@gmail.com>
2025-06-25 14:56:29 +0200

    fix: transition

---
+++ src/core/utils/merge-class-name/transition.ts
@@ -0,0 +1,18 @@
+import type { Transition } from '../../types';
+
+export const getTransition = (transition: Transition) => {
+  const transitions = transition.split('-');
+
+  if (transitions[0] === 'fade') {
+    return 'fade';
+  } else if (transitions[0] === 'collapse') {
+    return transition;
+  }
+
+  return transitions[transitions.length - 1] === 'fade'
+    ? 'transform-fade'
+    : 'transform';
+};
+
+export const getSpinnerTransition = (spin: boolean, isDisabled: boolean) =>
+  !spin || isDisabled ? 'rotate' : 'spin';
