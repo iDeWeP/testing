f884160557af2b18dd69075e92b7ddc970354b42
Richard Wagner <richard.wagner.sd@gmail.com>
2025-07-01 19:17:31 +0200

    fix: box

--- src/routes/core/unstyledBox/BorderExample.tsx
+++ src/routes/core/unstyledBox/BorderExample.tsx
@@ -5,14 +5,54 @@ import { ExampleUnstyledBox } from './ExampleUnstyledBox';
 export const BorderExample = () => (
   <ExampleSection title="Border">
     <ExampleCard>
-      <ExampleUnstyledBox border={false}>FALSE</ExampleUnstyledBox>
-      <ExampleUnstyledBox border="all">ALL</ExampleUnstyledBox>
-      <ExampleUnstyledBox border="x">X</ExampleUnstyledBox>
-      <ExampleUnstyledBox border="y">Y</ExampleUnstyledBox>
-      <ExampleUnstyledBox border="top">TOP</ExampleUnstyledBox>
-      <ExampleUnstyledBox border="bottom">BOTTOM</ExampleUnstyledBox>
-      <ExampleUnstyledBox border="left">LEFT</ExampleUnstyledBox>
-      <ExampleUnstyledBox border="right">RIGHT</ExampleUnstyledBox>
+      <ExampleUnstyledBox
+        light
+        border={false}
+      >
+        FALSE
+      </ExampleUnstyledBox>
+      <ExampleUnstyledBox
+        light
+        border="all"
+      >
+        ALL
+      </ExampleUnstyledBox>
+      <ExampleUnstyledBox
+        light
+        border="x"
+      >
+        X
+      </ExampleUnstyledBox>
+      <ExampleUnstyledBox
+        light
+        border="y"
+      >
+        Y
+      </ExampleUnstyledBox>
+      <ExampleUnstyledBox
+        light
+        border="top"
+      >
+        TOP
+      </ExampleUnstyledBox>
+      <ExampleUnstyledBox
+        light
+        border="bottom"
+      >
+        BOTTOM
+      </ExampleUnstyledBox>
+      <ExampleUnstyledBox
+        light
+        border="left"
+      >
+        LEFT
+      </ExampleUnstyledBox>
+      <ExampleUnstyledBox
+        light
+        border="right"
+      >
+        RIGHT
+      </ExampleUnstyledBox>
     </ExampleCard>
   </ExampleSection>
 );
