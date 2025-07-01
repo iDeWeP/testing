a0032cd35731be914d9cdb8ea8a5059cb2cb8260
Richard Wagner <richard.wagner.sd@gmail.com>
2025-07-01 11:06:41 +0200

    fix: typo

--- src/routes/core/unstyledTypography/Page.tsx
+++ src/routes/core/unstyledTypography/Page.tsx
@@ -1,9 +1,15 @@
 import { ExampleTitle } from '../../../components/ExampleTitle';
 import { AlignExample } from './AlignExample';
+import { BorderExample } from './BorderExample';
+import { ColorExample } from './ColorExample';
 import { DefaultExample } from './DefaultExample';
+import { MarginExample } from './MarginExample';
 import { OverflowExample } from './OverflowExample';
+import { RadiusExample } from './RadiusExample';
+import { ScaleExample } from './ScaleExample';
 import { SizeExample } from './SizeExample';
 import { UnderlineExample } from './UnderlineExample';
+import { VariantExample } from './VariantExample';
 import { WeightExample } from './WeightExample';
 import { WordBreakExample } from './WordBreakExample';
 import { WrapExample } from './WrapExample';
@@ -19,6 +25,12 @@ const Page = () => (
     <WrapExample />
     <AlignExample />
     <OverflowExample />
+    <VariantExample />
+    <ScaleExample />
+    <MarginExample />
+    <BorderExample />
+    <RadiusExample />
+    <ColorExample />
   </>
 );
 
