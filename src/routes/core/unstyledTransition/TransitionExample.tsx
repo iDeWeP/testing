e22443b124d4a768cb02600dd6288e99a7f31d39
Richard Wagner <richard.wagner.sd@gmail.com>
2025-07-01 14:17:59 +0200

    fix: transition

--- src/routes/core/unstyledTransition/TransitionExample.tsx
+++ src/routes/core/unstyledTransition/TransitionExample.tsx
@@ -3,15 +3,27 @@ import { ExampleUnstyledTransition } from './ExampleUnstyledTransition';
 
 export const TransitionExample = () => (
   <ExampleSection title="Transition">
-    <ExampleUnstyledTransition transition="fade">FADE</ExampleUnstyledTransition>
-    <ExampleUnstyledTransition transition="grow">GROW</ExampleUnstyledTransition>
-    <ExampleUnstyledTransition transition="grow-fade">GROW-FADE</ExampleUnstyledTransition>
-    <ExampleUnstyledTransition transition="slide-top">SLIDE-TOP</ExampleUnstyledTransition>
+    <ExampleUnstyledTransition transition="fade">
+      FADE
+    </ExampleUnstyledTransition>
+    <ExampleUnstyledTransition transition="grow">
+      GROW
+    </ExampleUnstyledTransition>
+    <ExampleUnstyledTransition transition="grow-fade">
+      GROW-FADE
+    </ExampleUnstyledTransition>
+    <ExampleUnstyledTransition transition="slide-top">
+      SLIDE-TOP
+    </ExampleUnstyledTransition>
     <ExampleUnstyledTransition transition="slide-bottom">
       SLIDE-BOTTOM
     </ExampleUnstyledTransition>
-    <ExampleUnstyledTransition transition="slide-left">SLIDE-LEFT</ExampleUnstyledTransition>
-    <ExampleUnstyledTransition transition="slide-right">SLIDE-RIGHT</ExampleUnstyledTransition>
+    <ExampleUnstyledTransition transition="slide-left">
+      SLIDE-LEFT
+    </ExampleUnstyledTransition>
+    <ExampleUnstyledTransition transition="slide-right">
+      SLIDE-RIGHT
+    </ExampleUnstyledTransition>
     <ExampleUnstyledTransition transition="slide-top-fade">
       SLIDE-TOP-FADE
     </ExampleUnstyledTransition>
@@ -24,8 +36,12 @@ export const TransitionExample = () => (
     <ExampleUnstyledTransition transition="slide-right-fade">
       SLIDE-RIGHT-FADE
     </ExampleUnstyledTransition>
-    <ExampleUnstyledTransition transition="collapse-x">COLLAPSE-X</ExampleUnstyledTransition>
-    <ExampleUnstyledTransition transition="collapse-y">COLLAPSE-Y</ExampleUnstyledTransition>
+    <ExampleUnstyledTransition transition="collapse-x">
+      COLLAPSE-X
+    </ExampleUnstyledTransition>
+    <ExampleUnstyledTransition transition="collapse-y">
+      COLLAPSE-Y
+    </ExampleUnstyledTransition>
     <ExampleUnstyledTransition transition="collapse-x-fade">
       COLLAPSE-X-FADE
     </ExampleUnstyledTransition>
