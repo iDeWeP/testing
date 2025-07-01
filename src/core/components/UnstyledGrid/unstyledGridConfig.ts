5b0abf0faa6bb7ee6cbc961e50b299e1622213d8
Richard Wagner <richard.wagner.sd@gmail.com>
2025-07-01 10:45:09 +0200

    fix: grid

--- src/core/components/UnstyledGrid/unstyledGridConfig.ts
+++ src/core/components/UnstyledGrid/unstyledGridConfig.ts
@@ -4,13 +4,6 @@ import type { UnstyledGridConfigProps } from './UnstyledGrid.types';
 export const unstyledGridConfig: ComponentConfig<UnstyledGridConfigProps> = {
   props: {
     as: 'div',
-    variant: 'none',
-    size: 'none',
-    scale: 'normal',
-    margin: 'none',
-    border: false,
-    radius: 'none',
-    color: 'neutral',
     flow: 'row',
     justifyContent: 'normal',
     alignContent: 'normal',
@@ -18,6 +11,13 @@ export const unstyledGridConfig: ComponentConfig<UnstyledGridConfigProps> = {
     alignItems: 'stretch',
     gap: 'none',
     colGap: 'none',
-    rowGap: 'none'
+    rowGap: 'none',
+    variant: 'none',
+    size: 'none',
+    scale: 'normal',
+    margin: 'none',
+    border: false,
+    radius: 'none',
+    color: 'neutral'
   }
 };
