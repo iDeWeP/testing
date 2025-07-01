a055594f66ae2270d647f71a26e58b6d0f2c443d
Richard Wagner <richard.wagner.sd@gmail.com>
2025-07-01 11:37:08 +0200

    fix: transition

--- src/core/components/UnstyledTransition/unstyledTransitionConfig.ts
+++ src/core/components/UnstyledTransition/unstyledTransitionConfig.ts
@@ -1,22 +1,61 @@
-import type { ComponentConfig } from '../../types';
+import type { CSSProps, ComponentConfig } from '../../types';
 import type { UnstyledTransitionConfigProps } from './UnstyledTransition.types';
 
-export const unstyledTransitionConfig: ComponentConfig<UnstyledTransitionConfigProps> =
-  {
-    props: {
-      as: 'div',
-      in: false,
-      unmountOnExit: false,
-      peak: 'auto',
-      transition: 'fade',
-      duration: 1000,
-      transitionProps: {},
-      variant: 'none',
-      size: 'none',
-      scale: 'normal',
-      margin: 'none',
-      border: false,
-      radius: 'none',
-      color: 'neutral'
-    }
+type Styles = {
+  styles: {
+    transition: Record<string, CSSProps>;
   };
+};
+
+export const unstyledTransitionConfig: ComponentConfig<UnstyledTransitionConfigProps> &
+  Styles = {
+  props: {
+    as: 'div',
+    in: false,
+    unmountOnExit: false,
+    peak: 'auto',
+    transition: 'fade',
+    duration: 1000,
+    transitionProps: {},
+    variant: 'none',
+    size: 'none',
+    scale: 'normal',
+    margin: 'none',
+    border: false,
+    radius: 'none',
+    color: 'neutral'
+  },
+  styles: {
+    transition: {
+      fade: {
+        transition: 'transition-opacity'
+      },
+      transform: {
+        transition: 'transition-transform'
+      },
+      'transform-fade': {
+        transition: 'transition-[transform,_opacity]'
+      },
+      'collapse-x': {
+        display: 'flex',
+        overflow: 'overflow-hidden',
+        textWrap: 'text-nowrap',
+        transition: 'transition-width'
+      },
+      'collapse-y': {
+        overflow: 'overflow-hidden',
+        transition: 'transition-height'
+      },
+      'collapse-x-fade': {
+        display: 'flex',
+        overflow: 'overflow-hidden',
+        textWrap: 'text-nowrap',
+        transition: 'transition-[width,_opacity]'
+      },
+      'collapse-y-fade': {
+        overflow: 'overflow-hidden',
+        transition: 'transition-[height,_opacity]'
+      }
+    }
+  }
+};
--- src/core/utils/merge-class-name/style-unstyled-transition.ts
+++ src/core/utils/merge-class-name/style-unstyled-transition.ts
@@ -1,4 +1,4 @@
-import { generic } from '../../config/generic';
+import { unstyledTransitionConfig } from '../../components/UnstyledTransition/unstyledTransitionConfig';
 import type { Classes } from '../../types';
 import { mergeClasses } from './merge-classes';
 import { getTransition } from './transition';
@@ -7,4 +7,7 @@ export const styleUnstyledTransition = (
   className: string,
   { transition = 'fade' }: Classes
 ) =>
-  mergeClasses(generic.styles.transition[getTransition(transition)], className);
+  mergeClasses(
+    unstyledTransitionConfig.styles.transition[getTransition(transition)],
+    className
+  );
