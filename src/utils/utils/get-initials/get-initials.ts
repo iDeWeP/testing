39e875ba74f2a4d7d5dcdfcf72703676b0748b32
Richard Wagner <richard.wagner.sd@gmail.com>
2025-07-02 18:13:24 +0200

    fix: initials

--- src/utils/utils/get-initials/get-initials.ts
+++ src/utils/utils/get-initials/get-initials.ts
@@ -1,10 +1,10 @@
 import type { ReactNode } from 'react';
 
 export const getInitials = (children: ReactNode) =>
-  typeof children === 'string'
+  typeof children !== 'string'
     ? children
+    : children
         .trim()
         .split(/\s+/)
         .map((word) => word[0].toUpperCase())
-        .join('')
-    : children;
+        .join('');
