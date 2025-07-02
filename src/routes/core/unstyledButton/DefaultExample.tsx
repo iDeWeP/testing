3fba84be3b19f82c7350260b7a2d549e96d3f3a0
Richard Wagner <richard.wagner.sd@gmail.com>
2025-07-02 11:05:45 +0200

    fix: default

--- src/routes/core/unstyledButton/DefaultExample.tsx
+++ src/routes/core/unstyledButton/DefaultExample.tsx
@@ -6,31 +6,19 @@ export const DefaultExample = () => (
   <ExampleSection title="Default">
     <ExampleCard>
       <ExampleUnstyledButton clear />
-      <ExampleUnstyledButton
-        clear
-        leftDecorator="LEFT-DECORATOR"
-      >
+      <ExampleUnstyledButton leftDecorator="LEFT-DECORATOR">
+        UNSTYLED-BUTTON
+      </ExampleUnstyledButton>
+      <ExampleUnstyledButton rightDecorator="RIGHT-DECORATOR">
         UNSTYLED-BUTTON
       </ExampleUnstyledButton>
       <ExampleUnstyledButton
-        clear
-        rightDecorator="RIGHT-DECORATOR"
-      >
-        UNSTYLED-BUTTON
-      </ExampleUnstyledButton>
-      <ExampleUnstyledButton
-        clear
         leftDecorator="LEFT-DECORATOR"
         rightDecorator="RIGHT-DECORATOR"
       >
         UNSTYLED-BUTTON
       </ExampleUnstyledButton>
-      <ExampleUnstyledButton
-        clear
-        ripple="none"
-      >
-        RIPPLE-NONE
-      </ExampleUnstyledButton>
+      <ExampleUnstyledButton ripple="none">RIPPLE-NONE</ExampleUnstyledButton>
     </ExampleCard>
   </ExampleSection>
 );
