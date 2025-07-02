8f98eb91c705861b40cc971217ae3e44777b3c8f
Richard Wagner <richard.wagner.sd@gmail.com>
2025-07-02 19:52:00 +0200

    fix: paper

---
+++ src/core/utils/merge-class-name/style-unstyled-paper.ts
@@ -0,0 +1,22 @@
+import { generic } from '../../config/generic';
+import type { Classes } from '../../types';
+import { getDirection } from './get-direction';
+import { mergeClasses } from './merge-classes';
+
+export const styleUnstyledPaper = (
+  className: string,
+  {
+    orientation = 'horizontal',
+    justifyContent = 'normal',
+    alignItems = 'stretch',
+    gap = 'none'
+  }: Classes
+) =>
+  mergeClasses(
+    generic.styles.display.flex,
+    generic.styles.direction[getDirection(orientation)],
+    generic.styles.justifyContent[justifyContent],
+    generic.styles.alignItems[alignItems],
+    generic.styles.gap.default[gap],
+    className
+  );
