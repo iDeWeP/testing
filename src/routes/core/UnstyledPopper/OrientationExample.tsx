730ac02a12828ef09ecee7942e294fe457792a5e
Richard Wagner <richard.wagner.sd@gmail.com>
2025-07-02 10:48:10 +0200

    fix: popper

---
+++ src/routes/core/UnstyledPopper/OrientationExample.tsx
@@ -0,0 +1,27 @@
+import { ExampleCard } from '../../../components/ExampleCard';
+import { ExampleSection } from '../../../components/ExampleSection';
+import { UnstyledBox } from '../../../core/components/UnstyledBox/UnstyledBox';
+import { ExampleUnstyledPopper } from './ExampleUnstyledPopper';
+
+export const OrientationExample = () => (
+  <ExampleSection title="Orientation">
+    <ExampleCard>
+      <ExampleUnstyledPopper
+        clear
+        orientation="horizontal"
+      >
+        <UnstyledBox variant="light">HORIZONTAL #1</UnstyledBox>
+        <UnstyledBox variant="light">HORIZONTAL #2</UnstyledBox>
+        <UnstyledBox variant="light">HORIZONTAL #3</UnstyledBox>
+      </ExampleUnstyledPopper>
+      <ExampleUnstyledPopper
+        clear
+        orientation="vertical"
+      >
+        <UnstyledBox variant="light">VERTICAL #1</UnstyledBox>
+        <UnstyledBox variant="light">VERTICAL #2</UnstyledBox>
+        <UnstyledBox variant="light">VERTICAL #3</UnstyledBox>
+      </ExampleUnstyledPopper>
+    </ExampleCard>
+  </ExampleSection>
+);
--- src/routes/core/UnstyledPopper/Page.tsx
+++ src/routes/core/UnstyledPopper/Page.tsx
@@ -1,26 +1,36 @@
 import { ExampleTitle } from '../../../components/ExampleTitle';
+import { AlignExample } from './AlignExample';
+import { BorderExample } from './BorderExample';
 import { CloseOnEscExample } from './CloseOnEscExample';
 import { CloseOnOutClickExample } from './CloseOnOutClick';
 import { CollisionExample } from './CollisionExample';
+import { ColorExample } from './ColorExample';
 import { DefaultExample } from './DefaultExample';
 import { DurationExample } from './DurationExample';
 import { FocusOnCloseExample } from './FocusOnClose';
 import { FocusOnOpenExample } from './FocusOnOpen';
 import { FocusTrapExample } from './FocusTrapExample';
 import { FollowCursorExample } from './FollowCursorExample';
+import { GapExample } from './GapExample';
+import { JustifyExample } from './JustifyExample';
 import { LockScrollExample } from './LockScrollExample';
+import { MarginExample } from './MarginExample';
 import { OffsetExample } from './OffsetExample';
+import { OrientationExample } from './OrientationExample';
 import { PeakExample } from './PeakExample';
 import { PlacementExample } from './PlacementExample';
 import { PortalExample } from './PortalExample';
+import { RadiusExample } from './RadiusExample';
+import { ScaleExample } from './ScaleExample';
+import { SizeExample } from './SizeExample';
 import { UnmountOnExitExample } from './UnmountOnExitExample';
+import { VariantExample } from './VariantExample';
 import { ZIndexExample } from './ZIndexExample';
 
 const Page = () => (
   <>
-    <ExampleTitle>Popper</ExampleTitle>
+    <ExampleTitle>UnstyledPopper</ExampleTitle>
     <DefaultExample />
-    <PlacementExample />
     <CollisionExample />
     <FollowCursorExample />
     <CloseOnOutClickExample />
@@ -36,6 +46,17 @@ const Page = () => (
     <ZIndexExample />
     <DurationExample />
     <PortalExample />
+    <OrientationExample />
+    <JustifyExample />
+    <AlignExample />
+    <GapExample />
+    <VariantExample />
+    <SizeExample />
+    <ScaleExample />
+    <MarginExample />
+    <BorderExample />
+    <RadiusExample />
+    <ColorExample />
   </>
 );
 
