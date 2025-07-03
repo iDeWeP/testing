f60171d6ee464f9e5425f4c6f22c8c87cdcfaed4
Richard Wagner <richard.wagner.sd@gmail.com>
2025-07-03 21:42:42 +0200

    fix: collapse

--- src/core/utils/style-collapse/style-collapse.ts
+++ src/core/utils/style-collapse/style-collapse.ts
@@ -1,31 +1,28 @@
 import type { RefObject } from 'react';
 import type { Animation } from '../../../hooks/hooks/use-animation/use-animation';
-import type { Peak, Scale } from '../../types';
+import type { Peak } from '../../types';
 
 export const styleCollapse = (
   { isEntering, isEntered }: Animation,
   peak: Peak,
   transitions: string[],
   ref: RefObject<HTMLElement | null>,
-  isIn: boolean,
-  scale: Scale
+  isIn: boolean
 ) => {
   const isHorizontal = transitions[1] === 'x';
   const dimension = isHorizontal ? 'width' : 'height';
 
   if ((!isIn && isEntered) || isEntering) {
     const sizes = ref.current?.firstElementChild?.getBoundingClientRect();
-    const width = scale === 'full' ? '100%' : sizes?.width ?? 'auto';
-    const length = isHorizontal ? width : sizes?.height ?? 'auto';
+    const length = isHorizontal
+      ? sizes?.width ?? 'auto'
+      : sizes?.height ?? 'auto';
 
     return { [dimension]: peak === 'auto' ? length : peak };
   }
 
   if (isEntered) {
-    const width = scale === 'full' ? '100%' : 'auto';
-    const length = isHorizontal ? width : 'auto';
-
-    return { [dimension]: peak === 'auto' ? length : peak };
+    return { [dimension]: peak === 'auto' ? 'auto' : peak };
   }
 
   return { [dimension]: '0px' };
