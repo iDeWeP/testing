ef6b3d9f2b04fa7197ee5d7fe4b3d2ae99c740c2
Richard Wagner <richard.wagner.sd@gmail.com>
2025-07-03 21:08:11 +0200

    fix: text

--- src/core/components/Text/textConfig.ts
+++ src/core/components/Text/textConfig.ts
@@ -1,11 +1,39 @@
 import type { ComponentConfig } from '../../types';
-import type { TextDefaultProps } from './Text.types';
+import type { TextConfigProps } from './Text.types';
 
-export const textConfig: ComponentConfig<TextDefaultProps> = {
+export const textConfig: ComponentConfig<TextConfigProps> = {
   props: {
     as: 'p',
     variant: 'text',
     font: 'md',
-    color: 'surface'
+    color: 'surface',
+    padding: 'none',
+    px: 'none',
+    py: 'none',
+    pt: 'none',
+    pb: 'none',
+    pl: 'none',
+    pr: 'none',
+    margin: 'none',
+    mx: 'none',
+    my: 'none',
+    mt: 'none',
+    mb: 'none',
+    ml: 'none',
+    mr: 'none',
+    border: false,
+    radius: 'none',
+    rt: 'none',
+    rb: 'none',
+    rl: 'none',
+    rr: 'none',
+    rtl: 'none',
+    rtr: 'none',
+    rbl: 'none',
+    rbr: 'none',
+    shadow: 'none',
+    gap: 'none',
+    gx: 'none',
+    gy: 'none'
   }
 };
