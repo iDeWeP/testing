ed6d73eabcc1ba868c6d26a8b5d39f1641781d1d
Richard Wagner <richard.wagner.sd@gmail.com>
2025-07-03 21:38:58 +0200

    fix: color

--- src/core/utils/merge-class-name/color.ts
+++ src/core/utils/merge-class-name/color.ts
@@ -1,14 +1,45 @@
-import type { Color } from '../../types';
+import type { Color, Variant } from '../../types';
+import { hasVariantBg } from './variant';
+
+const variants = {
+  plain: (color: Color) => ({
+    bgColor: 'none',
+    textColor: `${color}-on`
+  }),
+  text: (color: Color) => ({
+    bgColor: 'none',
+    textColor: color
+  }),
+  light: (color: Color) => ({
+    bgColor: `${color}-light`,
+    textColor: color
+  }),
+  surface: (color: Color) => ({
+    bgColor: `${color}-on`,
+    textColor: color
+  }),
+  solid: (color: Color) => ({
+    bgColor: color,
+    textColor: `${color}-on`
+  })
+};
 
 export const getColor = (
+  variant: Variant,
   color: Color,
   isDisabled?: boolean,
   isValid?: boolean,
   isInvalid?: boolean
-) => (isDisabled ? 'disabled' : getValidityColor(color, isValid, isInvalid));
+) => {
+  if (isDisabled) {
+    return {
+      bgColor: hasVariantBg(variant) ? 'disabled-light' : 'none',
+      textColor: 'disabled'
+    };
+  }
 
-export const getValidityColor = (
-  color: Color,
-  isValid?: boolean,
-  isInvalid?: boolean
-) => (isValid ? 'success' : isInvalid ? 'error' : color);
+  return variants[variant](getStateColor(color, isValid, isInvalid));
+};
+
+const getStateColor = (color: Color, isValid?: boolean, isInvalid?: boolean) =>
+  isValid ? 'success' : isInvalid ? 'error' : color;
