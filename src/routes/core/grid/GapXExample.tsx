0d6ce61481197aa3b1d13e8193684c4790a47446
Richard Wagner <richard.wagner.sd@gmail.com>
2025-07-03 23:49:06 +0200

    fix; gap

--- src/routes/core/grid/GapXExample.tsx
+++ src/routes/core/grid/GapXExample.tsx
@@ -1,3 +1,4 @@
+import { ExampleBox } from '../../../components/ExampleBox';
 import { ExampleCard } from '../../../components/ExampleCard';
 import { ExampleSection } from '../../../components/ExampleSection';
 import { Example } from './Example';
@@ -5,13 +6,41 @@ import { Example } from './Example';
 export const GapXExample = () => (
   <ExampleSection title="Gap X">
     <ExampleCard>
-      <Example gx="none">NONE</Example>
-      <Example gx="xs">XS</Example>
-      <Example gx="sm">SM</Example>
-      <Example gx="md">MD</Example>
-      <Example gx="lg">LG</Example>
-      <Example gx="xl">XL</Example>
-      <Example gx="xxl">XXL</Example>
+      <Example gx="none">
+        <ExampleBox>NONE #1</ExampleBox>
+        <ExampleBox>NONE #2</ExampleBox>
+        <ExampleBox>NONE #3</ExampleBox>
+      </Example>
+      <Example gx="xs">
+        <ExampleBox>XS #1</ExampleBox>
+        <ExampleBox>XS #2</ExampleBox>
+        <ExampleBox>XS #3</ExampleBox>
+      </Example>
+      <Example gx="sm">
+        <ExampleBox>SM #1</ExampleBox>
+        <ExampleBox>SM #2</ExampleBox>
+        <ExampleBox>SM #3</ExampleBox>
+      </Example>
+      <Example gx="md">
+        <ExampleBox>MD #1</ExampleBox>
+        <ExampleBox>MD #2</ExampleBox>
+        <ExampleBox>MD #3</ExampleBox>
+      </Example>
+      <Example gx="lg">
+        <ExampleBox>LG #1</ExampleBox>
+        <ExampleBox>LG #2</ExampleBox>
+        <ExampleBox>LG #3</ExampleBox>
+      </Example>
+      <Example gx="xl">
+        <ExampleBox>XL #1</ExampleBox>
+        <ExampleBox>XL #2</ExampleBox>
+        <ExampleBox>XL #3</ExampleBox>
+      </Example>
+      <Example gx="xxl">
+        <ExampleBox>XXL #1</ExampleBox>
+        <ExampleBox>XXL #2</ExampleBox>
+        <ExampleBox>XXL #3</ExampleBox>
+      </Example>
     </ExampleCard>
   </ExampleSection>
 );
--- src/routes/core/grid/GapYExample.tsx
+++ src/routes/core/grid/GapYExample.tsx
@@ -1,3 +1,4 @@
+import { ExampleBox } from '../../../components/ExampleBox';
 import { ExampleCard } from '../../../components/ExampleCard';
 import { ExampleSection } from '../../../components/ExampleSection';
 import { Example } from './Example';
@@ -5,13 +6,41 @@ import { Example } from './Example';
 export const GapYExample = () => (
   <ExampleSection title="Gap Y">
     <ExampleCard>
-      <Example gy="none">NONE</Example>
-      <Example gy="xs">XS</Example>
-      <Example gy="sm">SM</Example>
-      <Example gy="md">MD</Example>
-      <Example gy="lg">LG</Example>
-      <Example gy="xl">XL</Example>
-      <Example gy="xxl">XXL</Example>
+      <Example gy="none">
+        <ExampleBox>NONE #1</ExampleBox>
+        <ExampleBox>NONE #2</ExampleBox>
+        <ExampleBox>NONE #3</ExampleBox>
+      </Example>
+      <Example gy="xs">
+        <ExampleBox>XS #1</ExampleBox>
+        <ExampleBox>XS #2</ExampleBox>
+        <ExampleBox>XS #3</ExampleBox>
+      </Example>
+      <Example gy="sm">
+        <ExampleBox>SM #1</ExampleBox>
+        <ExampleBox>SM #2</ExampleBox>
+        <ExampleBox>SM #3</ExampleBox>
+      </Example>
+      <Example gy="md">
+        <ExampleBox>MD #1</ExampleBox>
+        <ExampleBox>MD #2</ExampleBox>
+        <ExampleBox>MD #3</ExampleBox>
+      </Example>
+      <Example gy="lg">
+        <ExampleBox>LG #1</ExampleBox>
+        <ExampleBox>LG #2</ExampleBox>
+        <ExampleBox>LG #3</ExampleBox>
+      </Example>
+      <Example gy="xl">
+        <ExampleBox>XL #1</ExampleBox>
+        <ExampleBox>XL #2</ExampleBox>
+        <ExampleBox>XL #3</ExampleBox>
+      </Example>
+      <Example gy="xxl">
+        <ExampleBox>XXL #1</ExampleBox>
+        <ExampleBox>XXL #2</ExampleBox>
+        <ExampleBox>XXL #3</ExampleBox>
+      </Example>
     </ExampleCard>
   </ExampleSection>
 );
