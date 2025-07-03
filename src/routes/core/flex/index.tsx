99b2dd95b15d84a56cf8712ed47657e77bd09167
Richard Wagner <richard.wagner.sd@gmail.com>
2025-07-03 22:43:24 +0200

    fix: flex

--- src/routes/core/flex/index.tsx
+++ src/routes/core/flex/index.tsx
@@ -1,7 +1,7 @@
 import { lazy } from 'react';
 import type { RouteObject } from 'react-router';
 
-export const UnstyledFlexRoute: RouteObject = {
-  path: '/core/unstyled-flex',
+export const FlexRoute: RouteObject = {
+  path: '/core/flex',
   Component: lazy(() => import('./Page'))
 };
