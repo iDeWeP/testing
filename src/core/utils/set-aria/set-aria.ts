52615add110a16396baa5005bb446d45d75434db
Richard Wagner <richard.wagner.sd@gmail.com>
2025-07-03 09:25:41 +0200

    fix: aria

--- src/core/utils/set-aria/set-aria.ts
+++ src/core/utils/set-aria/set-aria.ts
@@ -1,9 +1,9 @@
 import type { ElementType } from 'react';
 
 const types = {
-  button: (element: ElementType) =>
+  button: (element?: ElementType) =>
     element === 'button' ? {} : { tabIndex: 0, role: 'button' }
 };
 
-export const setAria = (type: keyof typeof types, element: ElementType) =>
+export const setAria = (type: keyof typeof types, element?: ElementType) =>
   types[type](element);
