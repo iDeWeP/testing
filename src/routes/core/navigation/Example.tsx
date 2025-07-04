b5b4a9675055fcf8b052d25ffddee40a1ab2e842
Richard Wagner <richard.wagner.sd@gmail.com>
2025-07-04 11:58:38 +0200

    fix: navigation

---
+++ src/routes/core/navigation/Example.tsx
@@ -0,0 +1,7 @@
+import { Navigation } from '../../../core/components/Navigation/Navigation';
+import type { NavigationProps } from '../../../core/components/Navigation/Navigation.types';
+
+export const Example = ({
+  children = 'NAVIGATION',
+  ...restProps
+}: NavigationProps<'div'>) => <Navigation {...restProps}>{children}</Navigation>;
