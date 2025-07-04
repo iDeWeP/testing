33a15602653b64ea61702224b1d3dcf3d158d97e
Richard Wagner <richard.wagner.sd@gmail.com>
2025-07-04 07:03:44 +0200

    foix :stack

--- src/core/utils/merge-class-name/style-stack.ts
+++ src/core/utils/merge-class-name/style-stack.ts
@@ -2,4 +2,4 @@ import { sharedStyles } from '../../config/shared-styles';
 import { mergeClasses } from './merge-classes';
 
 export const styleStack = (className: string) =>
-  mergeClasses(sharedStyles.stack, className);
+  mergeClasses(sharedStyles.display.flex, sharedStyles.stack, className);
