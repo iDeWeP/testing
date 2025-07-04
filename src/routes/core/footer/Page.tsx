3a2f0a11514559c04ba0e27ba745f02a89d0c108
Richard Wagner <richard.wagner.sd@gmail.com>
2025-07-04 08:40:49 +0200

    fix: footer

--- src/routes/core/footer/Page.tsx
+++ src/routes/core/footer/Page.tsx
@@ -1,12 +1,14 @@
 import { ExampleTitle } from '../../../components/ExampleTitle';
 import { DefaultExample } from './DefaultExample';
 import { GapExample } from './GapExample';
+import { OrientationExample } from './OrientationExample';
 
 const Page = () => (
   <>
     <ExampleTitle>Footer</ExampleTitle>
     <DefaultExample />
     <GapExample />
+    <OrientationExample />
   </>
 );
 
