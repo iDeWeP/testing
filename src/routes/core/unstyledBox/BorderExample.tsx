bba0d7e2caa96c235de5bf3c3b29a0fbccf7d9f7
Richard Wagner <richard.wagner.sd@gmail.com>
2025-07-04 22:24:31 +0200

    fix: box

--- src/routes/core/unstyledBox/BorderExample.tsx
+++ src/routes/core/unstyledBox/BorderExample.tsx
@@ -6,6 +6,9 @@ export const BorderExample = () => (
   <ExampleSection title="Border">
     <ExampleCard>
       <Example border="unset">UNSET</Example>
+      <Example border="none">NONE</Example>
+      <Example border="auto">AUTO</Example>
+      <Example border="set">SET</Example>
     </ExampleCard>
   </ExampleSection>
 );
