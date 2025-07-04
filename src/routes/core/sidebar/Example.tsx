2e777d0fc8732286512ce8d63edfd31613253440
Richard Wagner <richard.wagner.sd@gmail.com>
2025-07-04 11:55:35 +0200

    fxi: sidebar

--- src/routes/core/sidebar/Example.tsx
+++ src/routes/core/sidebar/Example.tsx
@@ -2,6 +2,6 @@ import { Sidebar } from '../../../core/components/Sidebar/Sidebar';
 import type { SidebarProps } from '../../../core/components/Sidebar/Sidebar.types';
 
 export const Example = ({
-  children = 'SECTION',
+  children = 'SIDEBAR',
   ...restProps
 }: SidebarProps<'div'>) => <Sidebar {...restProps}>{children}</Sidebar>;
