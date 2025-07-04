c875707cef315995443d3671552ab3ae8ecac484
Richard Wagner <richard.wagner.sd@gmail.com>
2025-07-04 12:30:23 +0200

    fix: grid

--- src/routes/core/unstyledText/index.tsx
+++
@@ -1,7 +0,0 @@
-import { lazy } from 'react';
-import type { RouteObject } from 'react-router';
-
-export const TextRoute: RouteObject = {
-  path: '/core/text',
-  Component: lazy(() => import('./Page'))
-};
--- src/routes/core/unstyledTypography/index.tsx
+++
@@ -1,7 +0,0 @@
-import { lazy } from 'react';
-import type { RouteObject } from 'react-router';
-
-export const UnstyledTypographyRoute: RouteObject = {
-  path: '/core/unstyled-typography',
-  Component: lazy(() => import('./Page'))
-};
