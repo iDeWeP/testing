6d81fe576c4d45576f2e9987a0f893524612294e
Richard Wagner <richard.wagner.sd@gmail.com>
2025-07-05 13:03:38 +0200

    fix: image

--- src/routes/core/image/DefaultExample.tsx
+++ src/routes/core/image/DefaultExample.tsx
@@ -1,11 +1,12 @@
 import { ExampleCard } from '../../../components/ExampleCard';
 import { ExampleSection } from '../../../components/ExampleSection';
 import { Example } from './Example';
+import imagePath from '../../../assets/images/photo.png';
 
 export const DefaultExample = () => (
   <ExampleSection title="Default">
     <ExampleCard>
-      <Example />
+      <Example src={imagePath} />
     </ExampleCard>
   </ExampleSection>
 );
--- src/routes/core/image/RadiusExample.tsx
+++ src/routes/core/image/RadiusExample.tsx
@@ -1,17 +1,43 @@
 import { ExampleCard } from '../../../components/ExampleCard';
 import { ExampleSection } from '../../../components/ExampleSection';
 import { Example } from './Example';
+import imagePath from '../../../assets/images/photo.png';
 
 export const RadiusExample = () => (
   <ExampleSection title="Radius">
     <ExampleCard>
-      <Example radius="unset" />
-      <Example radius="xs" />
-      <Example radius="sm" />
-      <Example radius="md" />
-      <Example radius="lg" />
-      <Example radius="xl" />
-      <Example radius="xxl" />
+      <Example
+        radius="unset"
+        src={imagePath}
+      />
+      <Example
+        radius="xs"
+        src={imagePath}
+      />
+      <Example
+        radius="sm"
+        src={imagePath}
+      />
+      <Example
+        radius="md"
+        src={imagePath}
+      />
+      <Example
+        radius="lg"
+        src={imagePath}
+      />
+      <Example
+        radius="xl"
+        src={imagePath}
+      />
+      <Example
+        radius="xxl"
+        src={imagePath}
+      />
+      <Example
+        radius="full"
+        src={imagePath}
+      />
     </ExampleCard>
   </ExampleSection>
 );
