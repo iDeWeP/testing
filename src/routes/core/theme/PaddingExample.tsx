ebceae7b9cfca613f2357d2bd1c55d61b9a1337a
Richard Wagner <richard.wagner.sd@gmail.com>
2025-07-06 11:24:23 +0200

    fix: theme

---
+++ src/routes/core/theme/PaddingExample.tsx
@@ -0,0 +1,18 @@
+import { ExampleCard } from '../../../components/ExampleCard';
+import { ExampleSection } from '../../../components/ExampleSection';
+import { Example } from './Example';
+
+export const PaddingExample = () => (
+  <ExampleSection title="Padding">
+    <ExampleCard>
+      <Example padding="unset">UNSET</Example>
+      <Example padding="none">NONE</Example>
+      <Example padding="xs">XS</Example>
+      <Example padding="sm">SM</Example>
+      <Example padding="md">MD</Example>
+      <Example padding="lg">LG</Example>
+      <Example padding="xl">XL</Example>
+      <Example padding="xxl">XXL</Example>
+    </ExampleCard>
+  </ExampleSection>
+);
