019cb41ba6d67947f9f7ffa7253eb85897020a45
Richard Wagner <richard.wagner.sd@gmail.com>
2025-07-08 12:06:01 +0200

    fix; types

--- src/core/components/Anchor/anchorConfig.ts
+++ src/core/components/Anchor/anchorConfig.ts
@@ -21,7 +21,7 @@ export const anchorConfig: ComponentConfig<AnchorConfigProps> = {
     mb: 'unset',
     ml: 'unset',
     mr: 'unset',
-    border: 'unset',
+    border: 'auto',
     bx: 'unset',
     by: 'unset',
     bt: 'unset',
