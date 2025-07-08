0e937713830f9c5cab8fe3c94f1de72a444ec0a6
Richard Wagner <richard.wagner.sd@gmail.com>
2025-07-08 12:27:47 +0200

    fix: sidebar

--- src/core/components/Sidebar/sidebarConfig.ts
+++ src/core/components/Sidebar/sidebarConfig.ts
@@ -21,7 +21,7 @@ export const sidebarConfig: ComponentConfig<SidebarConfigProps> = {
     mb: 'unset',
     ml: 'unset',
     mr: 'unset',
-    border: 'unset',
+    border: 'auto',
     bx: 'unset',
     by: 'unset',
     bt: 'unset',
