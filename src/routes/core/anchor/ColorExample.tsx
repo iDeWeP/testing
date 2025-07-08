cd668909144b47d0b3467350c0ad3d499f1e8cb4
Richard Wagner <richard.wagner.sd@gmail.com>
2025-07-08 11:46:55 +0200

    fix: anchor

--- src/routes/core/anchor/ColorExample.tsx
+++ src/routes/core/anchor/ColorExample.tsx
@@ -15,5 +15,16 @@ export const ColorExample = () => (
       <Example color="warning">WARNING</Example>
       <Example color="error">ERROR</Example>
     </ExampleCard>
+    <ExampleCard>
+      <Example color="unset">UNSET</Example>
+      <Example color="surface-on">SURFACE-ON</Example>
+      <Example color="neutral-on">NEUTRAL-ON</Example>
+      <Example color="primary-on">PRIMARY-ON</Example>
+      <Example color="secondary-on">SECONDARY-ON</Example>
+      <Example color="tertiary-on">TERTIARY-ON</Example>
+      <Example color="success-on">SUCCESS-ON</Example>
+      <Example color="warning-on">WARNING-ON</Example>
+      <Example color="error-on">ERROR-ON</Example>
+    </ExampleCard>
   </ExampleSection>
 );
