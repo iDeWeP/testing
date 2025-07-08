f60aea477f04c5f68f4c8fd1eebfb3cb03fa6a51
Richard Wagner <richard.wagner.sd@gmail.com>
2025-07-08 12:43:57 +0200

    fix; list-item

--- src/routes/core/text/Page.tsx
+++ src/routes/core/text/Page.tsx
@@ -2,13 +2,11 @@ import { ExampleTitle } from '../../../components/ExampleTitle';
 import { ColorExample } from './ColorExample';
 import { DefaultExample } from './DefaultExample';
 import { FontExample } from './FontExample';
-import { VariantExample } from './VariantExample';
 
 const Page = () => (
   <>
     <ExampleTitle>Text</ExampleTitle>
     <DefaultExample />
-    <VariantExample />
     <FontExample />
     <ColorExample />
   </>
