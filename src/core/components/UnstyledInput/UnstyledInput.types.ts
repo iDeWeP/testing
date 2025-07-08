01fdc2ebe8788fb639c832abcabd6ca3c3f007ec
Richard Wagner <richard.wagner.sd@gmail.com>
2025-07-08 09:44:40 +0200

    fix; image

--- src/core/components/UnstyledInput/UnstyledInput.types.ts
+++ src/core/components/UnstyledInput/UnstyledInput.types.ts
@@ -1,42 +1,59 @@
-import type { RefObject, ReactNode } from 'react';
+import type { ComponentPropsWithRef, RefObject, ReactNode } from 'react';
 import type {
-  ComponentRefProps,
   InputVariant,
   InputSize,
-  Scale,
   Margin,
   Radius,
   Color,
   MergeProps
 } from '../../types';
 
+export type UnstyledInputContainerProps = ComponentPropsWithRef<'div'>;
+export type UnstyledInputLabelProps = ComponentPropsWithRef<'label'>;
+export type UnstyledInputDecoratorProps = ComponentPropsWithRef<'span'>;
+export type UnstyledInputFieldsetProps = ComponentPropsWithRef<'fieldset'>;
+export type UnstyledInputClearanceProps = ComponentPropsWithRef<'legend'>;
+
 export type UnstyledInputComponentsProps = {
-  container?: ComponentRefProps<'div'>;
-  label?: ComponentRefProps<'label'>;
-  leftDecorator?: ComponentRefProps<'span'>;
-  rightDecorator?: ComponentRefProps<'span'>;
-  fieldset?: ComponentRefProps<'fieldset'>;
-  clearance?: ComponentRefProps<'legend'>;
+  container?: UnstyledInputContainerProps;
+  label?: UnstyledInputLabelProps;
+  leftDecorator?: UnstyledInputDecoratorProps;
+  rightDecorator?: UnstyledInputDecoratorProps;
+  fieldset?: UnstyledInputFieldsetProps;
+  clearance?: UnstyledInputClearanceProps;
 };
 
 type OptionalProps = {
   inputRef?: RefObject<HTMLInputElement | null>;
   focused?: boolean;
   shifted?: boolean;
+  valid?: boolean;
+  invalid?: boolean;
+  disabled?: boolean;
   label?: ReactNode;
   leftDecorator?: ReactNode;
   rightDecorator?: ReactNode;
 };
 
 export type UnstyledInputDefaultProps = {
-  valid?: boolean;
-  invalid?: boolean;
-  disabled?: boolean;
   variant?: InputVariant;
   size?: InputSize;
-  scale?: Scale;
   margin?: Margin;
+  mx?: Margin;
+  my?: Margin;
+  mt?: Margin;
+  mb?: Margin;
+  ml?: Margin;
+  mr?: Margin;
   radius?: Radius;
+  rt?: Radius;
+  rb?: Radius;
+  rl?: Radius;
+  rr?: Radius;
+  rtl?: Radius;
+  rtr?: Radius;
+  rbl?: Radius;
+  rbr?: Radius;
   color?: Color;
   componentsProps?: UnstyledInputComponentsProps;
 };
@@ -44,6 +61,6 @@ export type UnstyledInputDefaultProps = {
 export type UnstyledInputConfigProps = UnstyledInputDefaultProps;
 
 export type UnstyledInputProps = MergeProps<
-  ComponentRefProps<'input'>,
+  ComponentPropsWithRef<'input'>,
   UnstyledInputDefaultProps & OptionalProps
 >;
--- src/core/utils/merge-class-name/style-image.ts
+++ src/core/utils/merge-class-name/style-image.ts
@@ -2,4 +2,4 @@ import { sharedStyles } from '../../config/shared-styles';
 import { mergeClasses } from './merge-classes';
 
 export const styleImage = (className: string) =>
-  mergeClasses(sharedStyles.image, className);
+  mergeClasses(sharedStyles.image.default, className);
