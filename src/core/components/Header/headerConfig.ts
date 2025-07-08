ee818bbc79c9e2edbdda91edf0d898918d42bd77
Richard Wagner <richard.wagner.sd@gmail.com>
2025-07-08 12:10:49 +0200

    fix: header

--- src/core/components/Header/headerConfig.ts
+++ src/core/components/Header/headerConfig.ts
@@ -21,7 +21,7 @@ export const headerConfig: ComponentConfig<HeaderConfigProps> = {
     mb: 'unset',
     ml: 'unset',
     mr: 'unset',
-    border: 'unset',
+    border: 'auto',
     bx: 'unset',
     by: 'unset',
     bt: 'unset',
