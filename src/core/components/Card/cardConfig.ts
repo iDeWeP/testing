a5999ba4ad68fa9ec222b4e30f5019a9c12d201e
Richard Wagner <richard.wagner.sd@gmail.com>
2025-07-08 12:04:54 +0200

    fix; card

--- src/core/components/Card/cardConfig.ts
+++ src/core/components/Card/cardConfig.ts
@@ -25,7 +25,7 @@ export const cardConfig: ComponentConfig<CardConfigProps> = {
     mb: 'unset',
     ml: 'unset',
     mr: 'unset',
-    border: 'unset',
+    border: 'auto',
     bx: 'unset',
     by: 'unset',
     bt: 'unset',
