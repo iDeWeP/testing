13f3ced11d8bb1b7032f367935e514b9924b58c0
Richard Wagner <richard.wagner.sd@gmail.com>
2025-07-08 12:25:50 +0200

    fix: navigation

--- src/core/components/Navigation/navigationConfig.ts
+++ src/core/components/Navigation/navigationConfig.ts
@@ -21,7 +21,7 @@ export const navigationConfig: ComponentConfig<NavigationConfigProps> = {
     mb: 'unset',
     ml: 'unset',
     mr: 'unset',
-    border: 'unset',
+    border: 'auto',
     bx: 'unset',
     by: 'unset',
     bt: 'unset',
