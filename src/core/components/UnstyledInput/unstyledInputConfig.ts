32e439ffe68b375770d9c1baf1778c62bd47c5cd
Richard Wagner <richard.wagner.sd@gmail.com>
2025-07-09 11:08:49 +0200

    fix: config

--- src/core/components/UnstyledInput/unstyledInputConfig.ts
+++ src/core/components/UnstyledInput/unstyledInputConfig.ts
@@ -1,40 +1,15 @@
 import type {
-  CSSProps,
+  InputVariant,
   InputSize,
-  SidePlacement,
-  DefaultInputVariant,
+  CSSProps,
   ComponentConfig
 } from '../../types';
 import type { UnstyledInputConfigProps } from './UnstyledInput.types';
 
-type Padding = 'default' | 'decorated';
-
 type Styles = {
   styles: {
-    root: {
-      default: CSSProps;
-      size: Record<InputSize, CSSProps>;
-    };
-    decorator: {
-      default: CSSProps;
-      spacing: Record<SidePlacement, Record<Padding, CSSProps>>;
-      outlined: Record<SidePlacement, CSSProps>;
-    };
-    fieldset: {
-      default: CSSProps;
-    };
-    input: {
-      default: CSSProps;
-    };
-    clearance: {
-      default: CSSProps;
-    };
     label: {
-      default: CSSProps;
-      variant: Record<DefaultInputVariant, Record<InputSize, CSSProps>>;
-    };
-    generic: {
-      variant: Record<DefaultInputVariant, CSSProps>;
+      variant: Record<InputVariant, Record<InputSize, CSSProps>>;
     };
   };
 };
@@ -42,119 +17,29 @@ type Styles = {
 export const unstyledInputConfig: ComponentConfig<UnstyledInputConfigProps> &
   Styles = {
   props: {
-    valid: false,
-    invalid: false,
-    disabled: false,
     variant: 'default',
     size: 'md',
-    scale: 'normal',
-    margin: 'none',
-    radius: 'md',
+    margin: 'unset',
+    mx: 'unset',
+    my: 'unset',
+    mt: 'unset',
+    mb: 'unset',
+    ml: 'unset',
+    mr: 'unset',
+    radius: 'unset',
+    rt: 'unset',
+    rb: 'unset',
+    rl: 'unset',
+    rr: 'unset',
+    rtl: 'unset',
+    rtr: 'unset',
+    rbl: 'unset',
+    rbr: 'unset',
     color: 'neutral',
     componentsProps: {}
   },
   styles: {
-    root: {
-      default: {
-        display: 'inline-flex',  // ???
-        font: 'text-md',
-        transition: 'transition-colors',
-        transitionDuration: 'duration-500',
-        transitionTimingFunction: 'ease-in-out',
-        group: 'group'
-      },
-      size: {
-        sm: {
-          height: 'h-10'
-        },
-        md: {
-          height: 'h-12'
-        },
-        lg: {
-          height: 'h-16'
-        }
-      }
-    },
-    decorator: {
-      default: {
-        display: 'inline-flex',
-        gap: 'gap-2'
-      },
-      spacing: {
-        left: {
-          default: {
-            padding: 'pl-3'
-          },
-          decorated: {
-            padding: 'pl-4 pr-3'
-          }
-        },
-        right: {
-          default: {
-            padding: 'pr-3'
-          },
-          decorated: {
-            padding: 'pl-3 pr-4'
-          }
-        }
-      },
-      outlined: {
-        left: {
-          margin: 'ml-px',
-          borderWidth: 'border-l',
-          group: 'group-[.focused]:ml-0 group-[.focused]:border-l-2'
-        },
-        right: {
-          margin: 'mr-px',
-          borderWidth: 'border-r',
-          group: 'group-[.focused]:mr-0 group-[.focused]:border-r-2'
-        }
-      }
-    },
-    fieldset: {
-      default: {
-        display: 'flex',
-        flexGrow: 'grow' // ???
-      }
-    },
-    input: {
-      default: {
-        width: 'w-full', // ???
-        margin: 'mx-1',
-        focus: 'focus:outline-none' // ???
-      }
-    },
-    clearance: {
-      default: {
-        display: 'hidden',
-        height: 'h-0',
-        padding: 'px-1',
-        font: 'text-sm',
-        textWrap: 'text-nowrap',
-        fill: 'fill-transparent',
-        color: 'text-transparent',
-        alignItems: 'align-center',
-        gap: 'gap-2',
-        pointerEvents: 'pointer-events-none',
-        userSelect: 'select-none',
-        group: 'group-[.shifted]:flex'
-      }
-    },
     label: {
-      default: {
-        display: 'flex',
-        position: 'absolute',
-        left: 'left-1',
-        textWrap: 'text-nowrap',
-        alignItems: 'align-center',
-        gap: 'gap-2',
-        transition: 'transition-all',
-        transitionDuration: 'duration-500',
-        transitionTimingFunction: 'ease-in-out',
-        pointerEvents: 'pointer-events-none',
-        userSelect: 'select-none',
-        group: 'group-[.shifted]:h-4 group-[.shifted]:text-sm'
-      },
       variant: {
         default: {
           sm: {
@@ -203,30 +88,6 @@ export const unstyledInputConfig: ComponentConfig<UnstyledInputConfigProps> &
             translate: '-translate-y-2/4',
             group: 'group-[.shifted]:-top-px'
           }
-        }
-      }
-    },
-    generic: {
-      variant: {
-        default: {
-          padding: 'pb-0.5',
-          margin: 'mb-px',
-          borderWidth: 'border-b',
-          alignItems: 'items-end',
-          group: 'group-[.focused]:mb-0 group-[.focused]:border-b-2'
-        },
-        light: {
-          padding: 'pb-0.5',
-          margin: 'mb-px',
-          borderWidth: 'border-b',
-          alignItems: 'items-end',
-          group: 'group-[.focused]:mb-0 group-[.focused]:border-b-2'
-        },
-        outlined: {
-          margin: 'my-px',
-          borderWidth: 'border-y',
-          alignItems: 'items-center',
-          group: 'group-[.focused]:m-0 group-[.focused]:border-y-2'
         }
       }
     }
