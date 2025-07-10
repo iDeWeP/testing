e1992878e5ffe42ee4767d760a6123ded732c7cc
Richard Wagner <richard.wagner.sd@gmail.com>
2025-07-10 23:20:39 +0200

    fix: types

--- src/core/types.ts
+++ src/core/types.ts
@@ -5,6 +5,17 @@ import type {
   CSSProperties
 } from 'react';
 
+export type ComponentRefProps<E extends ElementType> = Omit<
+  ComponentPropsWithRef<E>,
+  'color'
+>;
+
+export type ComponentConfig<P> = {
+  props: Required<P>;
+};
+
+export type SVGNodeProps = ReactElement<ComponentPropsWithRef<'svg'>>;
+
 export type MergeProps<D extends object, T extends object> = Omit<D, keyof T> &
   T;
 
@@ -14,53 +25,30 @@ export type MergeComponentProps<E extends ElementType, T> = Omit<
 > &
   T;
 
-export type ComponentRefProps<E extends ElementType> = Omit<
-  ComponentPropsWithRef<E>,
-  'offset' | 'scale' | 'radius' | 'color'
->;
-// 'size' 'rotate' | 'content'>
-
-export type ComponentConfig<P> = {
-  props: Required<P>;
-};
-
-export type SVGNodeProps = ReactElement<ComponentPropsWithRef<'svg'>>;
-
+export type Theme = 'light' | 'dark';
 export type Loading = boolean | 'left' | 'right';
 export type Collision = 'none' | 'shift' | 'flip';
 export type Trigger =
   | 'none'
   | 'hover'
   | 'click'
+  | 'focus'
   | 'none-hover'
   | 'none-click'
+  | 'none-focus'
   | 'hover-none'
   | 'hover-click'
+  | 'hover-focus'
   | 'click-none'
-  | 'click-hover';
+  | 'click-hover'
+  | 'click-focus'
+  | 'focus-none'
+  | 'focus-hover'
+  | 'focus-click';
 export type Peak = 'auto' | number;
-/* export type Layout =
-  | 'default'
-  | 'row'
-  | 'col'
-  | 'left'
-  | 'center'
-  | 'right'
-  | 'dashboard'
-  | 'dashboard-left'
-  | 'dashboard-center'
-  | 'dashboard-right'; */
-export type Variant = 'none' | 'plain' | 'text' | 'light' | 'surface' | 'solid';
-export type InputVariant =
-  | 'default'
-  | 'light'
-  | 'outlined'
-  | 'default-plain'
-  | 'light-plain'
-  | 'outlined-plain';
-export type DefaultInputVariant = 'default' | 'light' | 'outlined';
-export type Theme = 'light' | 'dark';
-export type Orientation = 'vertical' | 'horizontal';
+export type Variant = 'outlined' | 'text' | 'light' | 'solid';
+export type InputVariant = 'default' | 'light' | 'outlined';
+export type Orientation = 'row' | 'col';
 export type Placement =
   | 'top'
   | 'bottom'
@@ -75,41 +63,16 @@ export type Placement =
   | 'right-start'
   | 'right-end';
 export type DefaultPlacement = 'top' | 'bottom' | 'left' | 'right';
-/* export type InnerPlacement =
-  | 'top'
-  | 'bottom'
-  | 'left'
-  | 'right'
-  | 'top-left'
-  | 'top-right'
-  | 'bottom-left'
-  | 'bottom-right'; */
-/*export type CornerPlacement =
-  | 'top-left'
-  | 'top-right'
-  | 'bottom-left'
-  | 'bottom-right'; */
 export type CrossPlacement = 'start' | 'center' | 'end';
 export type SidePlacement = 'left' | 'right';
+export type RipplePlacement = 'normal' | 'inner';
 export type MainAxis = DefaultPlacement;
 export type CrossAxis = CrossPlacement;
-/* export type Offset =
-  | 'none'
-  | 'xs'
-  | 'sm'
-  | 'md'
-  | 'lg'
-  | 'xl'
-  | 'xs-xs'
-  | 'sm-sm'
-  | 'md-md'
-  | 'lg-lg'
-  | 'xl-xl'; */
 export type ArrowOffset = [number | string, number | string];
-// export type Overlap = 'square' | 'rounded' | 'circular';
-export type Size = 'none' | 'xs' | 'sm' | 'md' | 'lg' | 'xl' | 'xxl';
+export type Size = 'unset' | 'xs' | 'sm' | 'md' | 'lg' | 'xl' | 'xxl';
 export type DefaultSize = 'xs' | 'sm' | 'md' | 'lg' | 'xl' | 'xxl';
-export type BoxSize =
+export type CardSize =
+  | 'unset'
   | 'none'
   | 'xs'
   | 'sm'
@@ -124,12 +87,28 @@ export type BoxSize =
   | 'xl-xl'
   | 'xxl-xxl';
 export type InputSize = 'sm' | 'md' | 'lg';
-// export type TextareaSize = 'sm' | 'md' | 'lg' | 'full';
-export type Scale = 'normal' | 'fit' | 'full';
-export type SizeScale = 'normal' | 'text';
-export type ButtonScale = 'normal' | 'full' | 'fit' | 'square';
-// export type IconScale = 'default' | 'text' | 'square' | 'text-square';
+export type Padding =
+  | 'unset'
+  | 'none'
+  | 'xs'
+  | 'sm'
+  | 'md'
+  | 'lg'
+  | 'xl'
+  | 'xxl';
 export type Margin =
+  | 'unset'
+  | 'none'
+  | 'xs'
+  | 'sm'
+  | 'md'
+  | 'lg'
+  | 'xl'
+  | 'xxl';
+export type Border = 'unset' | 'none' | 'auto' | 'set';
+export type DefaultBorder = 'unset' | 'none' | 'set';
+export type Radius =
+  | 'unset'
   | 'none'
   | 'xs'
   | 'sm'
@@ -137,115 +116,9 @@ export type Margin =
   | 'lg'
   | 'xl'
   | 'xxl'
-  | 'xs-x'
-  | 'xs-y'
-  | 'xs-top'
-  | 'xs-bottom'
-  | 'xs-left'
-  | 'xs-right'
-  | 'xs-x-top'
-  | 'xs-x-bottom'
-  | 'xs-y-left'
-  | 'xs-y-right'
-  | 'xs-top-left'
-  | 'xs-top-right'
-  | 'xs-bottom-left'
-  | 'xs-bottom-right'
-  | 'sm-x'
-  | 'sm-y'
-  | 'sm-top'
-  | 'sm-bottom'
-  | 'sm-left'
-  | 'sm-right'
-  | 'sm-x-top'
-  | 'sm-x-bottom'
-  | 'sm-y-left'
-  | 'sm-y-right'
-  | 'sm-top-left'
-  | 'sm-top-right'
-  | 'sm-bottom-left'
-  | 'sm-bottom-right'
-  | 'md-x'
-  | 'md-y'
-  | 'md-top'
-  | 'md-bottom'
-  | 'md-left'
-  | 'md-right'
-  | 'md-x-top'
-  | 'md-x-bottom'
-  | 'md-y-left'
-  | 'md-y-right'
-  | 'md-top-left'
-  | 'md-top-right'
-  | 'md-bottom-left'
-  | 'md-bottom-right'
-  | 'lg-x'
-  | 'lg-y'
-  | 'lg-top'
-  | 'lg-bottom'
-  | 'lg-left'
-  | 'lg-right'
-  | 'lg-x-top'
-  | 'lg-x-bottom'
-  | 'lg-y-left'
-  | 'lg-y-right'
-  | 'lg-top-left'
-  | 'lg-top-right'
-  | 'lg-bottom-left'
-  | 'lg-bottom-right'
-  | 'xl-x'
-  | 'xl-y'
-  | 'xl-top'
-  | 'xl-bottom'
-  | 'xl-left'
-  | 'xl-right'
-  | 'xl-x-top'
-  | 'xl-x-bottom'
-  | 'xl-y-left'
-  | 'xl-y-right'
-  | 'xl-top-left'
-  | 'xl-top-right'
-  | 'xl-bottom-left'
-  | 'xl-bottom-right'
-  | 'xxl-x'
-  | 'xxl-y'
-  | 'xxl-top'
-  | 'xxl-bottom'
-  | 'xxl-left'
-  | 'xxl-right'
-  | 'xxl-x-top'
-  | 'xxl-x-bottom'
-  | 'xxl-y-left'
-  | 'xxl-y-right'
-  | 'xxl-top-left'
-  | 'xxl-top-right'
-  | 'xxl-bottom-left'
-  | 'xxl-bottom-right';
-export type Border =
-  | boolean
-  | 'all'
-  | 'x'
-  | 'y'
-  | 'top'
-  | 'bottom'
-  | 'left'
-  | 'right'
-  | 'x-top'
-  | 'x-bottom'
-  | 'y-left'
-  | 'y-right'
-  | 'top-left'
-  | 'top-right'
-  | 'bottom-left'
-  | 'bottom-right';
-export type Radius = 'none' | 'xs' | 'sm' | 'md' | 'lg' | 'xl' | 'xxl' | 'full';
-export type Weight = 'normal' | 'semi' | 'bold';
-export type Underline = 'normal' | 'none' | 'hover' | 'always';
-export type WordBreak = 'normal' | 'words' | 'all' | 'keep';
-export type TextWrap = 'wrap' | 'nowrap' | 'balance' | 'pretty';
-export type TextAlign = 'left' | 'center' | 'right' | 'justify';
-export type TextOverflow = 'clip' | 'truncate' | 'ellipsis';
+  | 'full';
 export type Color =
+  | 'unset'
   | 'surface'
   | 'neutral'
   | 'primary'
@@ -253,32 +126,29 @@ export type Color =
   | 'tertiary'
   | 'success'
   | 'warning'
-  | 'error';
-export type Direction = 'row' | 'col' | 'row-reverse' | 'col-reverse';
-export type Flow = 'row' | 'col' | 'dense' | 'row-dense' | 'col-dense';
-export type Wrap = 'nowrap' | 'wrap' | 'wrap-reverse';
-export type JustifyContent =
-  | 'normal'
-  | 'start'
-  | 'center'
-  | 'end'
-  | 'between'
-  | 'around'
-  | 'evenly'
-  | 'stretch';
-export type AlignContent =
-  | 'normal'
-  | 'start'
-  | 'center'
-  | 'end'
-  | 'between'
-  | 'around'
-  | 'evenly'
-  | 'stretch'
-  | 'baseline';
-export type JustifyItems = 'stretch' | 'start' | 'center' | 'end';
-export type AlignItems = 'stretch' | 'start' | 'center' | 'end' | 'baseline';
-export type Gap = 'none' | 'xs' | 'sm' | 'md' | 'lg' | 'xl' | 'xxl';
+  | 'error'
+  | 'surface-on'
+  | 'neutral-on'
+  | 'primary-on'
+  | 'secondary-on'
+  | 'tertiary-on'
+  | 'success-on'
+  | 'warning-on'
+  | 'error-on';
+export type TrailColor = Color | 'transparent';
+export type Shadow =
+  | 'unset'
+  | 'none'
+  | 'xs'
+  | 'sm'
+  | 'md'
+  | 'lg'
+  | 'xl'
+  | 'xxl';
+export type Font = 'unset' | 'xs' | 'sm' | 'md' | 'lg' | 'xl' | 'xxl';
+export type Gap = 'unset' | 'none' | 'xs' | 'sm' | 'md' | 'lg' | 'xl' | 'xxl';
+export type Effect = 'unset' | 'shadow';
+export type RippleEffect = 'normal';
 export type GrowTransition = 'grow' | 'grow-fade';
 export type SlideTransition =
   | 'slide-top'
@@ -294,9 +164,7 @@ export type CollapseTransition =
   | 'collapse-y'
   | 'collapse-x-fade'
   | 'collapse-y-fade';
-export type Effect = 'none' | 'shadow';
-export type RippleEffect = 'normal';
-// export type AccordionEffect = 'none' | 'opacity';
+export type PopperTransition = 'fade' | GrowTransition;
 export type Transition =
   | 'fade'
   | GrowTransition
@@ -309,7 +177,7 @@ export type Cursor = {
   y: number;
 };
 
-export type TransitionProps = {
+export type AnimationProps = {
   enterDuration?: number;
   exitDuration?: number;
   enterDelay?: number;
@@ -320,59 +188,72 @@ export type TransitionProps = {
   exitingStyle?: CSSProperties;
 };
 
+export type CollisionFunction = (placement: Placement) => void;
+
 export type Classes = {
+  inputType?: 'input' | 'textarea';
   theme?: Theme;
-  valid?: boolean;
-  invalid?: boolean;
   focused?: boolean;
   shifted?: boolean;
-  loading?: Loading;
+  valid?: boolean;
+  invalid?: boolean;
   disabled?: boolean;
-  spin?: boolean;
   stateful?: boolean;
   followCursor?: boolean;
   variant?: Variant;
   inputVariant?: InputVariant;
   orientation?: Orientation;
-  placement?: Placement;
   defaultPlacement?: DefaultPlacement;
   sidePlacement?: SidePlacement;
-  float?: boolean;
-  relative?: boolean;
+  ripplePlacement?: RipplePlacement;
   size?: Size;
   defaultSize?: DefaultSize;
+  cardSize?: CardSize;
   inputSize?: InputSize;
-  spacing?: BoxSize;
-  scale?: Scale;
-  sizeScale?: SizeScale;
-  buttonScale?: ButtonScale;
-  border?: Border;
-  radius?: Radius;
-  color?: Color;
+  resize?: boolean;
+  padding?: Padding;
+  px?: Padding;
+  py?: Padding;
+  pt?: Padding;
+  pb?: Padding;
+  pl?: Padding;
+  pr?: Padding;
   margin?: Margin;
-  weight?: Weight;
-  underline?: Underline;
-  wordBreak?: WordBreak;
-  textWrap?: TextWrap;
-  textAlign?: TextAlign;
-  textOverflow?: TextOverflow;
-  direction?: Direction;
-  flow?: Flow;
-  wrap?: Wrap;
-  justifyContent?: JustifyContent;
-  alignContent?: AlignContent;
-  justifyItems?: JustifyItems;
-  alignItems?: AlignItems;
-  gap?: Gap;
-  colGap?: Gap;
-  rowGap?: Gap;
+  mx?: Margin;
+  my?: Margin;
+  mt?: Margin;
+  mb?: Margin;
+  ml?: Margin;
+  mr?: Margin;
+  border?: Border;
+  bx?: DefaultBorder;
+  by?: DefaultBorder;
+  bt?: DefaultBorder;
+  bb?: DefaultBorder;
+  bl?: DefaultBorder;
+  br?: DefaultBorder;
+  radius?: Radius;
+  rt?: Radius;
+  rb?: Radius;
+  rl?: Radius;
+  rr?: Radius;
+  rtl?: Radius;
+  rtr?: Radius;
+  rbl?: Radius;
+  rbr?: Radius;
+  font?: Font;
+  color?: Color;
+  trailColor?: TrailColor;
+  shadow?: Shadow;
   invisible?: boolean;
   blur?: boolean;
-  effect?: Effect;
-  rippleEffect?: RippleEffect;
+  gap?: Gap;
+  gx?: Gap;
+  gy?: Gap;
   transition?: Transition;
-  decorated?: boolean;
+  rippleEffect?: RippleEffect;
   hasRipple?: boolean;
+  decorated?: boolean;
 };
 
 type CSSPropKeys =
