e2494259e5ccf610d4b5c31aa5b03695b45a3346
Richard Wagner <richard.wagner.sd@gmail.com>
2025-07-10 22:57:06 +0200

    fix: border

--- src/core/utils/merge-class-name/border.ts
+++ src/core/utils/merge-class-name/border.ts
@@ -1,45 +1,19 @@
-import type { Border, DefaultBorder, DefaultPlacement } from '../../types';
+import type { Variant, Border, DefaultBorder } from '../../types';
 
-const placementsMap = {
-  left: 'right',
-  right: 'left',
-  top: 'bottom',
-  bottom: 'top'
-};
-
-export const getBorder = (border: Border) =>
-  typeof border === 'string' ? border : border ? 'all' : 'none';
-
-export const isPopperBordered = (
-  placement: DefaultPlacement,
-  border: Border
-) => {
-  if (!border) {
-    return false;
-  }
-
-  if (
-    border === true ||
-    border === 'all' ||
-    (border.includes('x') && (placement === 'left' || placement === 'right')) ||
-    (border.includes('y') && (placement === 'top' || placement === 'bottom'))
-  ) {
-    return true;
-  }
-
-  return border.includes(placementsMap[placement]);
-};
+export const getBorder = (variant: Variant, border: Border) =>
+  variant === 'outlined' && border === 'auto' ? 'set' : border;
 
 export const isCheckboxBordered = (
-  border?: Border,
-  bx?: DefaultBorder,
-  by?: DefaultBorder,
-  bt?: DefaultBorder,
-  bb?: DefaultBorder,
-  bl?: DefaultBorder,
-  br?: DefaultBorder
+  border: Border,
+  bx: DefaultBorder,
+  by: DefaultBorder,
+  bt: DefaultBorder,
+  bb: DefaultBorder,
+  bl: DefaultBorder,
+  br: DefaultBorder,
+  variant?: Variant
 ) =>
-  border === 'auto' ||
+  (variant === 'outlined' && border === 'auto') ||
   border === 'set' ||
   bx === 'set' ||
   by === 'set' ||
