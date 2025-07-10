9bea9ef4d7def2bd6a1b694c21ef549277b8da6e
Richard Wagner <richard.wagner.sd@gmail.com>
2025-07-10 23:06:11 +0200

    fix: scale

--- src/core/utils/merge-class-name/scale.ts
+++ src/core/utils/merge-class-name/scale.ts
@@ -1,19 +1,2 @@
-import type { ButtonScale, Border } from '../../types';
-
-type GetButtonReturn = {
-  sizeScale: 'normal' | 'square';
-  spacingScale: 'normal' | 'square' | 'border' | 'border-square';
-};
-
-export const getButtonScale = (
-  scale: ButtonScale,
-  border: Border
-): GetButtonReturn => {
-  const sizeScale = scale === 'square' ? 'square' : 'normal';
-  const spacingScale = scale === 'square' ? 'border-square' : 'border';
-
-  return { sizeScale, spacingScale: border ? spacingScale : sizeScale };
-};
-
 export const getInputScale = (hasRipple: boolean) =>
   hasRipple ? 'normal' : 'text';
