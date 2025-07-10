ea801f551273dfd4ba489048250db8888a068359
Richard Wagner <richard.wagner.sd@gmail.com>
2025-07-10 23:07:44 +0200

    fix: spacing

--- src/core/utils/merge-class-name/spacing.ts
+++ src/core/utils/merge-class-name/spacing.ts
@@ -1,2 +1,7 @@
+import type { InputSize } from '../../types';
+
+export const getInputSize = (size: InputSize, resize: boolean) =>
+  resize ? 'full' : size;
+
 export const getInputSpacing = (decorated: boolean) =>
   decorated ? 'decorated' : 'default';
