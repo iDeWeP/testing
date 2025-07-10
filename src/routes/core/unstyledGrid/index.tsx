44e1fcd70cf5466db86db5dcfcf858d7682932e9
Richard Wagner <richard.wagner.sd@gmail.com>
2025-07-10 20:34:12 +0200

    fix: delete

--- src/routes/core/unstyledGrid/index.tsx
+++
@@ -1,7 +0,0 @@
-import { lazy } from 'react';
-import type { RouteObject } from 'react-router';
-
-export const UnstyledGridRoute: RouteObject = {
-  path: '/core/unstyled-grid',
-  Component: lazy(() => import('./Page'))
-};
--- src/utils/utils/combine-functions/combine-functions.ts
+++
@@ -1,9 +0,0 @@
-export const combineFunctions =
-  (...handlers: ((() => void | boolean) | undefined | false)[]) =>
-  () => {
-    for (const handler of handlers) {
-      if (handler && handler() === false) {
-        return;
-      }
-    }
-  };
