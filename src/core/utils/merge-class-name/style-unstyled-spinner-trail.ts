c90f7a9c334cd2aeca9ee0a11b985de44d03aaed
Richard Wagner <richard.wagner.sd@gmail.com>
2025-07-11 09:49:27 +0200

    fix: spinner

--- src/core/utils/merge-class-name/style-unstyled-spinner-trail.ts
+++ src/core/utils/merge-class-name/style-unstyled-spinner-trail.ts
@@ -3,6 +3,7 @@ import type { Classes } from '../../types';
 import { getColor } from './color';
 import { mergeClasses } from './merge-classes';
 import { isTrailTransparent } from './variant';
+
 export const styleUnstyledSpinnerTrail = (
   className: string,
   {
