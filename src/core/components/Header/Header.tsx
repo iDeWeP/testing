c9730230673660f2d8c93e9b0ec3e842e60721c1
Richard Wagner <richard.wagner.sd@gmail.com>
2025-07-11 08:14:07 +0200

    fix: header

--- src/core/components/Header/Header.tsx
+++ src/core/components/Header/Header.tsx
@@ -1,11 +1,11 @@
 import type { ElementType } from 'react';
 import { useMergeProps } from '../../hooks/use-merge-props/use-merge-props';
-import { UnstyledBox } from '../UnstyledBox/UnstyledBox';
+import { UnstyledList } from '../UnstyledList/UnstyledList';
 import type { HeaderProps } from './Header.types';
 import { headerConfig } from './headerConfig';
 
 export const Header = <E extends ElementType>(props: HeaderProps<E>) => {
   const mergedProps = useMergeProps('header', headerConfig.props, props);
 
-  return <UnstyledBox {...mergedProps} />;
+  return <UnstyledList {...mergedProps} />;
 };
