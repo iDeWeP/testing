f716600a88376e41f9bfefe6c1623e8ace43cf38
Richard Wagner <richard.wagner.sd@gmail.com>
2025-07-12 17:27:47 +0200

    fix: list

--- src/core/utils/set-variant/set-variant.ts
+++
@@ -1,8 +0,0 @@
-import type { Variant } from '../../types';
-
-export const setVariant = (variant: Variant) =>
-  variant === 'solid' || variant === 'plain'
-    ? 'plain'
-    : variant === 'none'
-    ? 'none'
-    : 'text';
