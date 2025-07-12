a8dae66d08e59d89f87c9977a852c26b54f578f8
Richard Wagner <richard.wagner.sd@gmail.com>
2025-07-12 19:11:56 +0200

    fix; button

--- src/core/components/UnstyledButton/UnstyledButton.tsx
+++ src/core/components/UnstyledButton/UnstyledButton.tsx
@@ -1,28 +1,23 @@
 import type { ElementType } from 'react';
 import { useTheme } from '../../hooks/use-theme/use-theme';
+import { getReversedColor } from '../../utils/merge-class-name/color';
 import { mergeClassName } from '../../utils/merge-class-name/merge-class-name';
 import { mergeProps } from '../../utils/merge-props/merge-props';
 import { setAria } from '../../utils/set-aria/set-aria';
-import { setVariant } from '../../utils/set-variant/set-variant';
+import { UnstyledContainer } from '../UnstyledContainer/UnstyledContainer';
 import { UnstyledRipple } from '../UnstyledRipple/UnstyledRipple';
 import { UnstyledSpinner } from '../UnstyledSpinner/UnstyledSpinner';
 import type { UnstyledButtonProps } from './UnstyledButton.types';
 import { unstyledButtonConfig } from './unstyledButtonConfig';
 
-export const UnstyledButton = <E extends ElementType = 'button'>(
+export const UnstyledButton = <E extends ElementType>(
   props: UnstyledButtonProps<E>
 ) => {
   const {
     as: Component,
+    checked,
     loading = false,
     disabled = false,
-    variant,
-    size,
-    scale: buttonScale,
-    margin,
-    border,
-    radius,
-    color,
     effect,
     className,
     componentsProps,
@@ -30,25 +25,20 @@ export const UnstyledButton = <E extends ElementType = 'button'>(
     leftDecorator,
     rightDecorator,
     children,
+    variant,
+    size,
+    color,
     ...restProps
   } = mergeProps(unstyledButtonConfig.props, props);
 
   const theme = useTheme();
 
-  const isRelative =
-    loading !== 'left' && loading !== 'right' && !disabled && ripple !== 'none';
-
   const mergedClassName = mergeClassName('unstyledButton', className, {
     theme,
+    checked,
     loading,
     disabled,
     variant,
-    relative: isRelative,
-    size,
-    buttonScale,
-    margin,
-    border,
-    radius,
     color,
     effect
   });
@@ -60,14 +50,20 @@ export const UnstyledButton = <E extends ElementType = 'button'>(
       disabled={disabled}
       spin
       value={75}
-      variant={setVariant(variant)}
+      variant="text"
       float={loading === true}
       size={size}
       thickness={4}
       scale="text"
-      margin="none"
-      border={false}
-      color={color}
+      margin="unset"
+      mx="unset"
+      my="unset"
+      mt="unset"
+      mb="unset"
+      ml="unset"
+      mr="unset"
+      border="unset"
+      color={getReversedColor(color)}
       componentsProps={{
         bar: componentsProps.bar,
         trail: componentsProps.trail,
@@ -79,11 +75,14 @@ export const UnstyledButton = <E extends ElementType = 'button'>(
   );
 
   return (
-    <Component
+    <UnstyledContainer
       disabled={disabled || !!loading}
       type="button"
+      variant={variant}
+      size={size}
+      color={color}
       className={mergedClassName}
-      {...setAria('button', Component)}
+      {...setAria('button', { element: Component })}
       {...restProps}
     >
       {loading && loading !== 'right' && spinnerNode}
@@ -95,11 +94,12 @@ export const UnstyledButton = <E extends ElementType = 'button'>(
         <UnstyledRipple
           stateful={false}
           variant={variant}
+          scale="inner"
           color={color}
           effect={ripple}
           {...componentsProps.ripple}
         />
       )}
-    </Component>
+    </UnstyledContainer>
   );
 };
