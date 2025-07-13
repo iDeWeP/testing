18c83126fab814eba48380ed8b8728fcc330a13d
Richard Wagner <richard.wagner.sd@gmail.com>
2025-07-13 18:40:38 +0200

    fix; box

---
+++ src/core/components/Box/Box.tsx
@@ -0,0 +1,11 @@
+import type { ElementType } from 'react';
+import { useMergeProps } from '../../hooks/use-merge-props/use-merge-props';
+import { UnstyledBox } from '../UnstyledBox/UnstyledBox';
+import type { BoxProps } from './Box.types';
+import { boxConfig } from './boxConfig';
+
+export const Box = <E extends ElementType>(props: BoxProps<E>) => {
+  const mergedProps = useMergeProps('box', boxConfig.props, props);
+
+  return <UnstyledBox {...mergedProps} />;
+};
---
+++ src/core/components/Box/boxConfig.ts
@@ -0,0 +1,45 @@
+import type { ComponentConfig } from '../../types';
+import type { BoxConfigProps } from './Box.types';
+
+export const boxConfig: ComponentConfig<BoxConfigProps> = {
+  props: {
+    as: 'div',
+    variant: 'solid',
+    padding: 'unset',
+    px: 'unset',
+    py: 'unset',
+    pt: 'unset',
+    pb: 'unset',
+    pl: 'unset',
+    pr: 'unset',
+    margin: 'unset',
+    mx: 'unset',
+    my: 'unset',
+    mt: 'unset',
+    mb: 'unset',
+    ml: 'unset',
+    mr: 'unset',
+    border: 'auto',
+    bx: 'unset',
+    by: 'unset',
+    bt: 'unset',
+    bb: 'unset',
+    bl: 'unset',
+    br: 'unset',
+    radius: 'unset',
+    rt: 'unset',
+    rb: 'unset',
+    rl: 'unset',
+    rr: 'unset',
+    rtl: 'unset',
+    rtr: 'unset',
+    rbl: 'unset',
+    rbr: 'unset',
+    font: 'unset',
+    color: 'unset',
+    shadow: 'unset',
+    gap: 'unset',
+    gx: 'unset',
+    gy: 'unset'
+  }
+};
