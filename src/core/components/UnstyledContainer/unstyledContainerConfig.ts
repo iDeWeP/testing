33fbe29e6a2f90b0dc68d946a05e3cfa2039a96e
Richard Wagner <richard.wagner.sd@gmail.com>
2025-07-13 15:03:45 +0200

    fix: container

--- src/core/components/UnstyledContainer/unstyledContainerConfig.ts
+++ src/core/components/UnstyledContainer/unstyledContainerConfig.ts
@@ -1,38 +1,57 @@
-import type { ComponentConfig } from '../../types';
+import type { CSSProps, ComponentConfig } from '../../types';
 import type { UnstyledContainerConfigProps } from './UnstyledContainer.types';
 
-export const unstyledContainerConfig: ComponentConfig<UnstyledContainerConfigProps> =
-  {
-    props: {
-      as: 'div',
-      variant: 'solid',
-      size: 'md',
-      scale: 'normal',
-      margin: 'unset',
-      mx: 'unset',
-      my: 'unset',
-      mt: 'unset',
-      mb: 'unset',
-      ml: 'unset',
-      mr: 'unset',
-      border: 'auto',
-      bx: 'unset',
-      by: 'unset',
-      bt: 'unset',
-      bb: 'unset',
-      bl: 'unset',
-      br: 'unset',
-      radius: 'unset',
-      rt: 'unset',
-      rb: 'unset',
-      rl: 'unset',
-      rr: 'unset',
-      rtl: 'unset',
-      rtr: 'unset',
-      rbl: 'unset',
-      rbr: 'unset',
-      color: 'unset',
-      shadow: 'unset',
-      gap: 'unset'
-    }
+type Styles = {
+  styles: {
+    root: {
+      default: CSSProps;
+    };
   };
+};
+
+export const unstyledContainerConfig: ComponentConfig<UnstyledContainerConfigProps> &
+  Styles = {
+  props: {
+    as: 'div',
+    variant: 'solid',
+    size: 'md',
+    scale: 'normal',
+    margin: 'unset',
+    mx: 'unset',
+    my: 'unset',
+    mt: 'unset',
+    mb: 'unset',
+    ml: 'unset',
+    mr: 'unset',
+    border: 'auto',
+    bx: 'unset',
+    by: 'unset',
+    bt: 'unset',
+    bb: 'unset',
+    bl: 'unset',
+    br: 'unset',
+    radius: 'unset',
+    rt: 'unset',
+    rb: 'unset',
+    rl: 'unset',
+    rr: 'unset',
+    rtl: 'unset',
+    rtr: 'unset',
+    rbl: 'unset',
+    rbr: 'unset',
+    color: 'unset',
+    shadow: 'unset',
+    gap: 'unset'
+  },
+  styles: {
+    root: {
+      default: {
+        display: 'inline-flex',
+        textWrap: 'text-nowrap',
+        justifyContent: 'justify-center',
+        alignItems: 'items-center',
+        userSelect: 'select-none'
+      }
+    }
+  }
+};
