b42796ea95e6acaf6e8d27e1446208f08dc59eaf
Richard Wagner <richard.wagner.sd@gmail.com>
2025-07-13 18:46:23 +0200

    fix: box

--- src/routes/core/box/Example.tsx
+++ src/routes/core/box/Example.tsx
@@ -2,6 +2,6 @@ import { Box } from '../../../core/components/Box/Box';
 import type { BoxProps } from '../../../core/components/Box/Box.types';
 
 export const Example = ({
-  children = 'UNSTYLED-BOX',
+  children = 'BOX',
   ...restProps
 }: BoxProps<'div'>) => <Box {...restProps}>{children}</Box>;
