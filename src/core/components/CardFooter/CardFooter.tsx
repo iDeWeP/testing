08c88b27a8723f4db1ec79e6daf4cbefc2bff2bb
Richard Wagner <richard.wagner.sd@gmail.com>
2025-07-14 15:00:52 +0200

    fix: footer

--- src/core/components/CardFooter/CardFooter.tsx
+++ src/core/components/CardFooter/CardFooter.tsx
@@ -1,5 +1,6 @@
 import type { ElementType } from 'react';
 import { useMergeProps } from '../../hooks/use-merge-props/use-merge-props';
+import { mergeClassName } from '../../utils/merge-class-name/merge-class-name';
 import { UnstyledCard } from '../UnstyledCard/UnstyledCard';
 import type { CardFooterProps } from './CardFooter.types';
 import { cardFooterConfig } from './cardFooterConfig';
@@ -7,11 +8,21 @@ import { cardFooterConfig } from './cardFooterConfig';
 export const CardFooter = <E extends ElementType>(
   props: CardFooterProps<E>
 ) => {
-  const mergedProps = useMergeProps(
+  const { className, padding, ...restProps } = useMergeProps(
     'cardFooter',
     cardFooterConfig.props,
     props
   );
 
-  return <UnstyledCard {...mergedProps} />;
+  const mergedClassName = mergeClassName('cardFooter', className, {
+    padding
+  });
+
+  return (
+    <UnstyledCard
+      padding="unset"
+      className={mergedClassName}
+      {...restProps}
+    />
+  );
 };
