91894f30f81cbf279c22f91655beed2855d8bdac
Richard Wagner <richard.wagner.sd@gmail.com>
2025-07-14 09:14:38 +0200

    fix: box

--- src/core/components/Box/Box.types.ts
+++ src/core/components/Box/Box.types.ts
@@ -1,62 +1,16 @@
-import type { ElementType, ComponentPropsWithRef } from 'react';
+import type { ElementType } from 'react';
+import type { MergeProps } from '../../types';
 import type {
-  Variant,
-  Padding,
-  Margin,
-  Border,
-  DefaultBorder,
-  Radius,
-  Font,
-  Color,
-  Shadow,
-  Gap,
-  MergeProps
-} from '../../types';
+  UnstyledBoxDefaultProps,
+  UnstyledBoxConfigProps,
+  UnstyledBoxProps
+} from '../UnstyledBox/UnstyledBox.types';
 
-export type BoxDefaultProps = {
-  as?: ElementType;
-  variant?: Variant;
-  padding?: Padding;
-  px?: Padding;
-  py?: Padding;
-  pt?: Padding;
-  pb?: Padding;
-  pl?: Padding;
-  pr?: Padding;
-  margin?: Margin;
-  mx?: Margin;
-  my?: Margin;
-  mt?: Margin;
-  mb?: Margin;
-  ml?: Margin;
-  mr?: Margin;
-  border?: Border;
-  bx?: DefaultBorder;
-  by?: DefaultBorder;
-  bt?: DefaultBorder;
-  bb?: DefaultBorder;
-  bl?: DefaultBorder;
-  br?: DefaultBorder;
-  radius?: Radius;
-  rt?: Radius;
-  rb?: Radius;
-  rl?: Radius;
-  rr?: Radius;
-  rtl?: Radius;
-  rtr?: Radius;
-  rbl?: Radius;
-  rbr?: Radius;
-  font?: Font;
-  color?: Color;
-  shadow?: Shadow;
-  gap?: Gap;
-  gx?: Gap;
-  gy?: Gap;
-};
+export type BoxDefaultProps = UnstyledBoxDefaultProps;
 
-export type BoxConfigProps = BoxDefaultProps;
+export type BoxConfigProps = UnstyledBoxConfigProps;
 
 export type BoxProps<E extends ElementType> = MergeProps<
-  ComponentPropsWithRef<E>,
+  UnstyledBoxProps<E>,
   BoxDefaultProps
 >;
