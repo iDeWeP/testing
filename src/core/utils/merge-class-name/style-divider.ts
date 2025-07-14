075381952bee61446afb4023cdb476fe3e6bd353
Richard Wagner <richard.wagner.sd@gmail.com>
2025-07-14 10:46:35 +0200

    fix: divider

--- src/core/utils/merge-class-name/style-divider.ts
+++ src/core/utils/merge-class-name/style-divider.ts
@@ -1,15 +1,22 @@
 import { dividerConfig } from '../../components/Divider/dividerConfig';
 import { sharedStyles } from '../../config/shared-styles';
+import { systemStyles } from '../../config/system-styles';
 import type { Classes } from '../../types';
 import { mergeClasses } from './merge-classes';
+import { getDividerPadding } from './spacing';
 
 export const styleDivider = (
   className: string,
-  { orientation = 'row', decorated = false }: Classes
-) =>
-  mergeClasses(
+  { orientation = 'row', padding = 'unset', decorated = false }: Classes
+) => {
+  const { x, y } = getDividerPadding(orientation, padding);
+
+  return mergeClasses(
     sharedStyles.display.flex,
     decorated && dividerConfig.styles.root.decorated,
     dividerConfig.styles.root.orientation[orientation],
+    systemStyles.padding.default.x[x],
+    systemStyles.padding.default.y[y],
     className
   );
+};
