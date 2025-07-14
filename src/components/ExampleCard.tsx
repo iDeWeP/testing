27bf633f2076ceeb77f59e414c2e68f082a4d168
Richard Wagner <richard.wagner.sd@gmail.com>
2025-07-14 14:03:17 +0200

    fix: divider

--- src/components/ExampleCard.tsx
+++ src/components/ExampleCard.tsx
@@ -3,10 +3,13 @@ import type { CardProps } from '../core/components/Card/Card.types';
 
 type Props = {
   col?: boolean;
+  height?: boolean;
 } & CardProps<'div'>;
 
-export const ExampleCard = ({ col, ...restProps }: Props) => {
-  const className = `relative bg-disabled-100 flex-wrap justify-evenly items-center
+export const ExampleCard = ({ col, height, ...restProps }: Props) => {
+  const className = `relative ${
+    height ? 'h-[100px]' : ''
+  } bg-disabled-100 flex-wrap justify-evenly items-center
   }`;
 
   return (
