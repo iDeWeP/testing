bce02f7064ae94cdedd6e8bea649931208595fda
Richard Wagner <richard.wagner.sd@gmail.com>
2025-07-14 10:08:49 +0200

    fix

--- src/routes/core/unstyledSpinner/MarginMExample.tsx
+++ src/routes/core/unstyledSpinner/MarginMExample.tsx
@@ -5,24 +5,24 @@ import { Example } from './Example';
 export const MarginMExample = () => (
   <ExampleSection title="Margin M">
     <ExampleCard>
-      <Example m="unset" />
-      <Example m="none" />
-      <Example m="xs" />
-      <Example m="sm" />
-      <Example m="md" />
-      <Example m="lg" />
-      <Example m="xl" />
-      <Example m="xxl" />
+      <Example m="unset">UNSET</Example>
+      <Example m="none">NONE</Example>
+      <Example m="xs">XS</Example>
+      <Example m="sm">SM</Example>
+      <Example m="md">MD</Example>
+      <Example m="lg">LG</Example>
+      <Example m="xl">XL</Example>
+      <Example m="xxl">XXL</Example>
     </ExampleCard>
     <ExampleCard>
-      <Example m="unset" />
-      <Example m="none" />
-      <Example m="xs-xs" />
-      <Example m="sm-sm" />
-      <Example m="md-md" />
-      <Example m="lg-lg" />
-      <Example m="xl-xl" />
-      <Example m="xxl-xxl" />
+      <Example m="unset">UNSET</Example>
+      <Example m="none">NONE</Example>
+      <Example m="xs-xs">XS-XS</Example>
+      <Example m="sm-sm">SM-SM</Example>
+      <Example m="md-md">MD-MD</Example>
+      <Example m="lg-lg">LG-LG</Example>
+      <Example m="xl-xl">XL-XL</Example>
+      <Example m="xxl-xxl">XXL-XXL</Example>
     </ExampleCard>
   </ExampleSection>
 );
