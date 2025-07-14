837c16e0bb714a0ae486216fab82447970a1e932
Richard Wagner <richard.wagner.sd@gmail.com>
2025-07-14 14:28:31 +0200

    fix: gap

--- src/routes/core/label/Page.tsx
+++ src/routes/core/label/Page.tsx
@@ -2,6 +2,7 @@ import { ExampleTitle } from '../../../components/ExampleTitle';
 import { ColorExample } from './ColorExample';
 import { DefaultExample } from './DefaultExample';
 import { FontExample } from './FontExample';
+import { GapExample } from './GapExample';
 import { PlacementExample } from './PlacementExample';
 
 const Page = () => (
@@ -11,6 +12,7 @@ const Page = () => (
     <PlacementExample />
     <FontExample />
     <ColorExample />
+    <GapExample />
   </>
 );
 
