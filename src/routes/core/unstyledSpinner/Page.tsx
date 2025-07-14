29650f7b886b5f21c06bd27cc4bf0c186b09f19c
Richard Wagner <richard.wagner.sd@gmail.com>
2025-07-14 10:08:28 +0200

    fix: margin

--- src/routes/core/unstyledSpinner/Page.tsx
+++ src/routes/core/unstyledSpinner/Page.tsx
@@ -1,22 +1,22 @@
-import { ExampleTitle } from "../../../components/ExampleTitle";
-import { BorderExample } from "./BorderExample";
-import { ColorExample } from "./ColorExample";
-import { DefaultExample } from "./DefaultExample";
-import { DisabledExample } from "./DisabledExample";
-import { FloatExample } from "./FloatExample";
-import { MarginBExample } from "./MarginBExample";
-import { MarginExample } from "./MarginExample";
-import { MarginLExample } from "./MarginLExample";
-import { MarginRExample } from "./MarginRExample";
-import { MarginTExample } from "./MarginTExample";
-import { MarginXExample } from "./MarginXExample";
-import { MarginYExample } from "./MarginYExample";
-import { SizeExample } from "./SizeExample";
-import { SpinExample } from "./SpinExample";
-import { ThicknessExample } from "./ThicknessExample";
-import { ValueExample } from "./ValueExample";
-import { VariantExample } from "./VariantExample";
-
+import { ExampleTitle } from '../../../components/ExampleTitle';
+import { BorderExample } from './BorderExample';
+import { ColorExample } from './ColorExample';
+import { DefaultExample } from './DefaultExample';
+import { DisabledExample } from './DisabledExample';
+import { FloatExample } from './FloatExample';
+import { MarginBExample } from './MarginBExample';
+import { MarginExample } from './MarginExample';
+import { MarginLExample } from './MarginLExample';
+import { MarginMExample } from './MarginMExample';
+import { MarginRExample } from './MarginRExample';
+import { MarginTExample } from './MarginTExample';
+import { MarginXExample } from './MarginXExample';
+import { MarginYExample } from './MarginYExample';
+import { SizeExample } from './SizeExample';
+import { SpinExample } from './SpinExample';
+import { ThicknessExample } from './ThicknessExample';
+import { ValueExample } from './ValueExample';
+import { VariantExample } from './VariantExample';
 
 const Page = () => (
   <>
@@ -30,6 +30,7 @@ const Page = () => (
     <SizeExample />
     <ThicknessExample />
     <MarginExample />
+    <MarginMExample />
     <MarginXExample />
     <MarginYExample />
     <MarginTExample />
