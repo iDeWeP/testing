b5a33dfd11ffb576b37a3ffbd852a2e11ff99fb9
Richard Wagner <richard.wagner.sd@gmail.com>
2025-07-14 14:29:46 +0200

    fix: gap

--- src/routes/core/label/GapExample.tsx
+++ src/routes/core/label/GapExample.tsx
@@ -1,4 +1,3 @@
-import { ExampleBox } from '../../../components/ExampleBox';
 import { ExampleCard } from '../../../components/ExampleCard';
 import { ExampleSection } from '../../../components/ExampleSection';
 import { Example } from './Example';
@@ -6,46 +5,14 @@ import { Example } from './Example';
 export const GapExample = () => (
   <ExampleSection title="Gap">
     <ExampleCard>
-      <Example gap="unset">
-        <ExampleBox>UNSET #1</ExampleBox>
-        <ExampleBox>UNSET #2</ExampleBox>
-        <ExampleBox>UNSET #3</ExampleBox>
-      </Example>
-      <Example gap="none">
-        <ExampleBox>NONE #1</ExampleBox>
-        <ExampleBox>NONE #2</ExampleBox>
-        <ExampleBox>NONE #3</ExampleBox>
-      </Example>
-      <Example gap="xs">
-        <ExampleBox>XS #1</ExampleBox>
-        <ExampleBox>XS #2</ExampleBox>
-        <ExampleBox>XS #3</ExampleBox>
-      </Example>
-      <Example gap="sm">
-        <ExampleBox>SM #1</ExampleBox>
-        <ExampleBox>SM #2</ExampleBox>
-        <ExampleBox>SM #3</ExampleBox>
-      </Example>
-      <Example gap="md">
-        <ExampleBox>MD #1</ExampleBox>
-        <ExampleBox>MD #2</ExampleBox>
-        <ExampleBox>MD #3</ExampleBox>
-      </Example>
-      <Example gap="lg">
-        <ExampleBox>LG #1</ExampleBox>
-        <ExampleBox>LG #2</ExampleBox>
-        <ExampleBox>LG #3</ExampleBox>
-      </Example>
-      <Example gap="xl">
-        <ExampleBox>XL #1</ExampleBox>
-        <ExampleBox>XL #2</ExampleBox>
-        <ExampleBox>XL #3</ExampleBox>
-      </Example>
-      <Example gap="xxl">
-        <ExampleBox>XXL #1</ExampleBox>
-        <ExampleBox>XXL #2</ExampleBox>
-        <ExampleBox>XXL #3</ExampleBox>
-      </Example>
+      <Example gap="unset">UNSET</Example>
+      <Example gap="none">NONE</Example>
+      <Example gap="xs">XS</Example>
+      <Example gap="sm">SM</Example>
+      <Example gap="md">MD</Example>
+      <Example gap="lg">LG</Example>
+      <Example gap="xl">XL</Example>
+      <Example gap="xxl">XXL</Example>
     </ExampleCard>
   </ExampleSection>
 );
