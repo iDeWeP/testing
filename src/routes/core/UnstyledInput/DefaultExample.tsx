a18c6e1246a31217dc372cf93defce52d5f6ca1b
Richard Wagner <richard.wagner.sd@gmail.com>
2025-07-15 17:31:21 +0200

    fix; input

--- src/routes/core/UnstyledInput/DefaultExample.tsx
+++ src/routes/core/UnstyledInput/DefaultExample.tsx
@@ -5,8 +5,8 @@ import { Example } from './Example';
 export const DefaultExample = () => (
   <ExampleSection title="Default">
     <ExampleCard>
+      <Example clear />
       <Example
-        clear
         leftDecorator="left"
         rightDecorator="right"
         label="label"
