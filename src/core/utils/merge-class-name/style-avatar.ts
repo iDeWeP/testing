a867e15628fe6df09db4106c92cfdbc22eaa08de
Richard Wagner <richard.wagner.sd@gmail.com>
2025-07-15 21:23:51 +0200

    fix; avatar

---
+++ src/core/utils/merge-class-name/style-avatar.ts
@@ -0,0 +1,15 @@
+import { sharedStyles } from '../../config/shared-styles';
+import { systemStyles } from '../../config/system-styles';
+import type { Classes } from '../../types';
+import { getColor } from './color';
+import { mergeClasses } from './merge-classes';
+
+export const styleAvatar = (
+  className: string,
+  { theme = 'light', ring = 'unset' }: Classes
+) =>
+  mergeClasses(
+    ring !== 'unset' && sharedStyles.ring.normal,
+    systemStyles.color.ring[theme][getColor('solid', ring).bg],
+    className
+  );
