54d525d390d59917116f4569635580bfd709ece7
Richard Wagner <richard.wagner.sd@gmail.com>
2025-07-15 21:10:51 +0200

    fix; title

--- src/core/components/Title/Title.tsx
+++ src/core/components/Title/Title.tsx
@@ -1,23 +1,20 @@
 import type { ElementType } from 'react';
 import { useMergeProps } from '../../hooks/use-merge-props/use-merge-props';
-import { mergeClassName } from '../../utils/merge-class-name/merge-class-name';
+import { getReversedFont } from '../../utils/merge-class-name/spacing';
 import { UnstyledBox } from '../UnstyledBox/UnstyledBox';
 import type { TitleProps } from './Title.types';
 import { titleConfig } from './titleConfig';
 
 export const Title = <E extends ElementType>(props: TitleProps<E>) => {
-  const { font, className, ...restProps } = useMergeProps(
+  const { font, ...restProps } = useMergeProps(
     'title',
     titleConfig.props,
     props
   );
 
-  const mergedClassName = mergeClassName('title', className, { font });
-
   return (
     <UnstyledBox
-      font="unset"
-      className={mergedClassName}
+      font={getReversedFont(font)}
       {...restProps}
     />
   );
