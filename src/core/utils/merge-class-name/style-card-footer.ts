032ea7dc9d7752f524878593b1439e63f0cf5a14
Richard Wagner <richard.wagner.sd@gmail.com>
2025-07-15 16:28:45 +0200

    fix: card-footer

---
+++ src/core/utils/merge-class-name/style-card-footer.ts
@@ -0,0 +1,31 @@
+import { systemStyles } from '../../config/system-styles';
+import type { Classes } from '../../types';
+import { getBorder } from './border';
+import { mergeClasses } from './merge-classes';
+import { getSpacing } from './spacing';
+
+export const styleCardFooter = (
+  className: string,
+  {
+    variant = 'solid',
+    padding = 'unset',
+    margin = 'unset',
+    border = 'unset',
+    radius = 'unset'
+  }: Classes
+) => {
+  const paddings = getSpacing(padding, true);
+  const margins = getSpacing(margin, true);
+  const defaultBorder = getBorder(variant, border);
+
+  return mergeClasses(
+    systemStyles.padding.default.x[paddings.x],
+    systemStyles.padding.default.b[paddings.y],
+    systemStyles.margin.x[margins.x],
+    systemStyles.margin.b[margins.y],
+    systemStyles.border.x[defaultBorder],
+    systemStyles.border.b[defaultBorder],
+    systemStyles.radius.b[radius],
+    className
+  );
+};
