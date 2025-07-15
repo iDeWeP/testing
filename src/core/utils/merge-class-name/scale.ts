f3dd6983cbd58294b2bcd152e5f8d39fe7069773
Richard Wagner <richard.wagner.sd@gmail.com>
2025-07-15 21:26:26 +0200

    fix; scale

--- src/core/utils/merge-class-name/scale.ts
+++ src/core/utils/merge-class-name/scale.ts
@@ -1,28 +1,18 @@
-import type { DefaultSize, Size, Scale } from '../../types';
-import { isSpacingInner, getDefaultSize } from './spacing';
+import type { Size, Scale } from '../../types';
+import { isSpacingInner } from './spacing';
 
 type GetScale = {
-  default: 'normal' | 'inner';
-  dimension: 'normal' | 'square';
-  size: string;
+  size: 'normal' | 'square';
   padding: 'normal' | 'square' | 'inner-normal' | 'inner-square';
-  font: DefaultSize;
-  isSquare: boolean;
 };
 
 export const getScale = (size: Size, scale: Scale): GetScale => {
-  const isInner = isSpacingInner(size);
-  const defaultSize = getDefaultSize(size);
   const padding =
     scale === 'circle' || scale === 'square' ? 'square' : 'normal';
 
   return {
-    default: isInner ? 'inner' : 'normal',
-    dimension: scale === 'square' ? 'square' : 'normal',
-    size: scale === 'none' ? 'unset' : defaultSize,
-    padding: isInner ? `inner-${padding}` : padding,
-    font: isInner ? 'xs' : defaultSize,
-    isSquare: scale === 'square'
+    size: scale === 'square' ? 'square' : 'normal',
+    padding: isSpacingInner(size) ? `inner-${padding}` : padding
   };
 };
 
