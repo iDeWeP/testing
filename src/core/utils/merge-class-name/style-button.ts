3b78f5d2473ac3c39f46700ee6e2fcb4ec503a28
Richard Wagner <richard.wagner.sd@gmail.com>
2025-07-16 00:01:28 +0200

    fix; buttonm

--- src/core/utils/merge-class-name/style-button.ts
+++ src/core/utils/merge-class-name/style-button.ts
@@ -6,7 +6,7 @@ import { getColor, getCheckableColor } from './color';
 import { mergeClasses } from './merge-classes';
 import { getFocusableState, getLoadingState } from './state';
 
-export const styleUnstyledButton = (
+export const styleButton = (
   className: string,
   {
     theme = 'light',
@@ -18,20 +18,16 @@ export const styleUnstyledButton = (
     effect = 'unset'
   }: Classes
 ) => {
-  const { bgColor, textColor } = getColor(
-    variant,
-    getCheckableColor(color, checked),
-    disabled
-  );
+  const colors = getColor(variant, getCheckableColor(color, checked), disabled);
 
   return mergeClasses(
     checked && unstyledButtonConfig.styles.root.checked,
     unstyledButtonConfig.styles.root.default,
     sharedStyles.focusable[getFocusableState(disabled, loading)],
-    disabled && systemStyles.color.bg[theme][bgColor],
-    disabled && systemStyles.color.text[theme][textColor],
-    disabled && systemStyles.color.fill[theme][textColor],
-    systemStyles.color.ring[theme][bgColor],
+    disabled && systemStyles.color.bg[theme][colors.bg],
+    disabled && systemStyles.color.text[theme][colors.text],
+    disabled && systemStyles.color.fill[theme][colors.text],
+    systemStyles.color.ring[theme][colors.bg],
     unstyledButtonConfig.styles.root.loading[getLoadingState(loading)],
     sharedStyles.effect[effect],
     className
