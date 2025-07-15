06dd1ac0c0b9c2d2ede98d71a51ffbd1c966a7ca
Richard Wagner <richard.wagner.sd@gmail.com>
2025-07-15 07:31:21 +0200

    fix; variant

--- src/core/utils/merge-class-name/variant.ts
+++ src/core/utils/merge-class-name/variant.ts
@@ -3,8 +3,8 @@ import type { Variant } from '../../types';
 export const hasVariantBg = (variant: Variant) =>
   variant === 'light' || variant === 'solid';
 
-export const setRippleVariant = (variant: Variant) =>
-  variant === 'light' ? 'light' : 'text';
-
 export const getCheckableVariant = (variant: Variant) =>
   variant === 'light' || variant === 'solid' ? 'light' : variant;
+
+export const setRippleVariant = (variant: Variant) =>
+  variant === 'light' ? 'light' : 'text';
