e8ed2f6d70907627db1fe12e044af4dbdb47246b
Richard Wagner <richard.wagner.sd@gmail.com>
2025-07-15 21:34:59 +0200

    fix: spinner

--- src/core/components/UnstyledSpinner/unstyledSpinnerConfig.ts
+++ src/core/components/UnstyledSpinner/unstyledSpinnerConfig.ts
@@ -1,13 +1,11 @@
-import type { CSSProps, ComponentConfig } from '../../types';
+import type { CSSProps, CSSStyles, ComponentConfig } from '../../types';
 import type { UnstyledSpinnerConfigProps } from './UnstyledSpinner.types';
-
-type Scale = 'normal' | 'inner';
 
 type Styles = {
   styles: {
     root: {
       default: CSSProps;
-      float: Record<Scale, Record<string, CSSProps>>;
+      float: CSSStyles;
       spin: CSSProps;
       rotate: CSSProps;
     };
@@ -43,57 +41,53 @@ export const unstyledSpinnerConfig: ComponentConfig<UnstyledSpinnerConfigProps> 
         color: 'fill-none'
       },
       float: {
-        normal: {
-          xs: {
-            top: 'top-[calc(50%_-_0.625rem)]',
-            left: 'left-[calc(50%_-_0.625rem)]'
-          },
-          sm: {
-            top: 'top-[calc(50%_-_1rem)]',
-            left: 'left-[calc(50%_-_1rem)]'
-          },
-          md: {
-            top: 'top-[calc(50%_-_1.25rem)]',
-            left: 'left-[calc(50%_-_1.25rem)]'
-          },
-          lg: {
-            top: 'top-[calc(50%_-_1.5rem)]',
-            left: 'left-[calc(50%_-_1.5rem)]'
-          },
-          xl: {
-            top: 'top-[calc(50%_-_2rem)]',
-            left: 'left-[calc(50%_-_2rem)]'
-          },
-          xxl: {
-            top: 'top-[calc(50%_-_2.5rem)]',
-            left: 'left-[calc(50%_-_2.5rem)]'
-          }
+        xs: {
+          top: 'top-[calc(50%_-_0.625rem)]',
+          left: 'left-[calc(50%_-_0.625rem)]'
         },
-        inner: {
-          xs: {
-            top: 'top-[calc(50%_-_0.375)]',
-            left: 'left-[calc(50%_-_0.375)]'
-          },
-          sm: {
-            top: 'top-[calc(50%_-_0.5rem)]',
-            left: 'left-[calc(50%_-_0.5rem)]'
-          },
-          md: {
-            top: 'top-[calc(50%_-_0.625rem)]',
-            left: 'left-[calc(50%_-_0.625rem)]'
-          },
-          lg: {
-            top: 'top-[calc(50%_-_0.75rem)]',
-            left: 'left-[calc(50%_-_0.75rem)]'
-          },
-          xl: {
-            top: 'top-[calc(50%_-_0.875rem)]',
-            left: 'left-[calc(50%_-_0.875rem)]'
-          },
-          xxl: {
-            top: 'top-[calc(50%_-_1rem)]',
-            left: 'left-[calc(50%_-_1rem)]'
-          }
+        sm: {
+          top: 'top-[calc(50%_-_1rem)]',
+          left: 'left-[calc(50%_-_1rem)]'
+        },
+        md: {
+          top: 'top-[calc(50%_-_1.25rem)]',
+          left: 'left-[calc(50%_-_1.25rem)]'
+        },
+        lg: {
+          top: 'top-[calc(50%_-_1.5rem)]',
+          left: 'left-[calc(50%_-_1.5rem)]'
+        },
+        xl: {
+          top: 'top-[calc(50%_-_2rem)]',
+          left: 'left-[calc(50%_-_2rem)]'
+        },
+        xxl: {
+          top: 'top-[calc(50%_-_2.5rem)]',
+          left: 'left-[calc(50%_-_2.5rem)]'
+        },
+        'xs-xs': {
+          top: 'top-[calc(50%_-_0.375)]',
+          left: 'left-[calc(50%_-_0.375)]'
+        },
+        'sm-sm': {
+          top: 'top-[calc(50%_-_0.5rem)]',
+          left: 'left-[calc(50%_-_0.5rem)]'
+        },
+        'md-md': {
+          top: 'top-[calc(50%_-_0.625rem)]',
+          left: 'left-[calc(50%_-_0.625rem)]'
+        },
+        'lg-lg': {
+          top: 'top-[calc(50%_-_0.75rem)]',
+          left: 'left-[calc(50%_-_0.75rem)]'
+        },
+        'xl-xl': {
+          top: 'top-[calc(50%_-_0.875rem)]',
+          left: 'left-[calc(50%_-_0.875rem)]'
+        },
+        'xxl-xxl': {
+          top: 'top-[calc(50%_-_1rem)]',
+          left: 'left-[calc(50%_-_1rem)]'
         }
       },
       spin: {
