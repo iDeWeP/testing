c49b12920cce6e66e62948f376dd8f48ffba3463
Richard Wagner <richard.wagner.sd@gmail.com>
2025-07-15 08:17:25 +0200

    fix; scale

--- src/routes/core/unstyledContainer/ScaleExample.tsx
+++ src/routes/core/unstyledContainer/ScaleExample.tsx
@@ -10,5 +10,31 @@ export const ScaleExample = () => (
       <Example scale="square">SQUARE</Example>
       <Example scale="circle">CIRCLE</Example>
     </ExampleCard>
+    <ExampleCard>
+      <Example
+        size="md-md"
+        scale="none"
+      >
+        NONE
+      </Example>
+      <Example
+        size="md-md"
+        scale="normal"
+      >
+        NORMAL
+      </Example>
+      <Example
+        size="md-md"
+        scale="square"
+      >
+        SQUARE
+      </Example>
+      <Example
+        size="md-md"
+        scale="circle"
+      >
+        CIRCLE
+      </Example>
+    </ExampleCard>
   </ExampleSection>
 );
