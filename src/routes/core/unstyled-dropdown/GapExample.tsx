156132b80033dedd11ec3c277c27106696583233
Richard Wagner <richard.wagner.sd@gmail.com>
2025-07-16 20:34:10 +0200

    fix: unstyled

--- src/routes/core/unstyled-dropdown/GapExample.tsx
+++ src/routes/core/unstyled-dropdown/GapExample.tsx
@@ -1,3 +1,4 @@
+import { ExampleBox } from '../../../components/ExampleBox';
 import { ExampleCard } from '../../../components/ExampleCard';
 import { ExampleSection } from '../../../components/ExampleSection';
 import { Example } from './Example';
@@ -5,24 +6,88 @@ import { Example } from './Example';
 export const GapExample = () => (
   <ExampleSection title="Gap">
     <ExampleCard>
-      <Example gap="unset">UNSET</Example>
-      <Example gap="none">NONE</Example>
-      <Example gap="xs">XS</Example>
-      <Example gap="sm">SM</Example>
-      <Example gap="md">MD</Example>
-      <Example gap="lg">LG</Example>
-      <Example gap="xl">XL</Example>
-      <Example gap="xxl">XXL</Example>
+      <Example gap="unset">
+        <ExampleBox>UNSET #1</ExampleBox>
+        <ExampleBox>UNSET #2</ExampleBox>
+        <ExampleBox>UNSET #3</ExampleBox>
+      </Example>
+      <Example gap="none">
+        <ExampleBox>NONE #1</ExampleBox>
+        <ExampleBox>NONE #2</ExampleBox>
+        <ExampleBox>NONE #3</ExampleBox>
+      </Example>
+      <Example gap="xs">
+        <ExampleBox>XS #1</ExampleBox>
+        <ExampleBox>XS #2</ExampleBox>
+        <ExampleBox>XS #3</ExampleBox>
+      </Example>
+      <Example gap="sm">
+        <ExampleBox>SM #1</ExampleBox>
+        <ExampleBox>SM #2</ExampleBox>
+        <ExampleBox>SM #3</ExampleBox>
+      </Example>
+      <Example gap="md">
+        <ExampleBox>MD #1</ExampleBox>
+        <ExampleBox>MD #2</ExampleBox>
+        <ExampleBox>MD #3</ExampleBox>
+      </Example>
+      <Example gap="lg">
+        <ExampleBox>LG #1</ExampleBox>
+        <ExampleBox>LG #2</ExampleBox>
+        <ExampleBox>LG #3</ExampleBox>
+      </Example>
+      <Example gap="xl">
+        <ExampleBox>XL #1</ExampleBox>
+        <ExampleBox>XL #2</ExampleBox>
+        <ExampleBox>XL #3</ExampleBox>
+      </Example>
+      <Example gap="xxl">
+        <ExampleBox>XXL #1</ExampleBox>
+        <ExampleBox>XXL #2</ExampleBox>
+        <ExampleBox>XXL #3</ExampleBox>
+      </Example>
     </ExampleCard>
     <ExampleCard>
-      <Example gap="unset">UNSET</Example>
-      <Example gap="none">NONE</Example>
-      <Example gap="xs-xs">XS-SX</Example>
-      <Example gap="sm-sm">SM-SM</Example>
-      <Example gap="md-md">MD-MD</Example>
-      <Example gap="lg-lg">LG-LG</Example>
-      <Example gap="xl-xl">XL-XL</Example>
-      <Example gap="xxl-xxl">XXL-XXL</Example>
+      <Example gap="unset">
+        <ExampleBox>UNSET #1</ExampleBox>
+        <ExampleBox>UNSET #2</ExampleBox>
+        <ExampleBox>UNSET #3</ExampleBox>
+      </Example>
+      <Example gap="none">
+        <ExampleBox>NONE #1</ExampleBox>
+        <ExampleBox>NONE #2</ExampleBox>
+        <ExampleBox>NONE #3</ExampleBox>
+      </Example>
+      <Example gap="xs-xs">
+        <ExampleBox>XS-XS #1</ExampleBox>
+        <ExampleBox>XS-XS #2</ExampleBox>
+        <ExampleBox>XS-XS #3</ExampleBox>
+      </Example>
+      <Example gap="sm-sm">
+        <ExampleBox>SM-SM #1</ExampleBox>
+        <ExampleBox>SM-SM #2</ExampleBox>
+        <ExampleBox>SM-SM #3</ExampleBox>
+      </Example>
+      <Example gap="md-md">
+        <ExampleBox>MD-MD #1</ExampleBox>
+        <ExampleBox>MD-MD #2</ExampleBox>
+        <ExampleBox>MD-MD #3</ExampleBox>
+      </Example>
+      <Example gap="lg-lg">
+        <ExampleBox>LG-LG #1</ExampleBox>
+        <ExampleBox>LG-LG #2</ExampleBox>
+        <ExampleBox>LG-LG #3</ExampleBox>
+      </Example>
+      <Example gap="xl-xl">
+        <ExampleBox>XL-XL #1</ExampleBox>
+        <ExampleBox>XL-XL #2</ExampleBox>
+        <ExampleBox>XL-XL #3</ExampleBox>
+      </Example>
+      <Example gap="xxl-xxl">
+        <ExampleBox>XXL-XXL #1</ExampleBox>
+        <ExampleBox>XXL-XXL #2</ExampleBox>
+        <ExampleBox>XXL-XXL #3</ExampleBox>
+      </Example>
     </ExampleCard>
   </ExampleSection>
 );
