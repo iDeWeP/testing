c7b981fc3430f07cdef6bb439b5842f3a96a6ce5
Richard Wagner <richard.wagner.sd@gmail.com>
2025-07-16 09:12:10 +0200

    fix; box

--- src/routes/core/unstyled-box/GapYExample.tsx
+++ src/routes/core/unstyled-box/GapYExample.tsx
@@ -14,5 +14,15 @@ export const GapYExample = () => (
       <Example gy="xl">XL</Example>
       <Example gy="xxl">XXL</Example>
     </ExampleCard>
+    <ExampleCard>
+      <Example gy="unset">UNSET</Example>
+      <Example gy="none">NONE</Example>
+      <Example gy="xs-xs">XS-SX</Example>
+      <Example gy="sm-sm">SM-SM</Example>
+      <Example gy="md-md">MD-MD</Example>
+      <Example gy="lg-lg">LG-LG</Example>
+      <Example gy="xl-xl">XL-XL</Example>
+      <Example gy="xxl-xxl">XXL-XXL</Example>
+    </ExampleCard>
   </ExampleSection>
 );
