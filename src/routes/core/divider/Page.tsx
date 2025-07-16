5ae2cc29f5d874325710c240e35f98c5787a9cfd
Richard Wagner <richard.wagner.sd@gmail.com>
2025-07-16 10:34:10 +0200

    fix: divider

--- src/routes/core/divider/Page.tsx
+++ src/routes/core/divider/Page.tsx
@@ -3,23 +3,8 @@ import { ColorExample } from './ColorExample';
 import { DefaultExample } from './DefaultExample';
 import { FontExample } from './FontExample';
 import { GapExample } from './GapExample';
-import { MarginBExample } from './MarginBExample';
-import { MarginExample } from './MarginExample';
-import { MarginLExample } from './MarginLExample';
-import { MarginMExample } from './MarginMExample';
-import { MarginRExample } from './MarginRExample';
-import { MarginTExample } from './MarginTExample';
-import { MarginXExample } from './MarginXExample';
-import { MarginYExample } from './MarginYExample';
 import { OrientationExample } from './OrientationExample';
-import { PaddingBExample } from './PaddingBExample';
 import { PaddingExample } from './PaddingExample';
-import { PaddingLExample } from './PaddingLExample';
-import { PaddingPExample } from './PaddingPExample';
-import { PaddingRExample } from './PaddingRExample';
-import { PaddingTExample } from './PaddingTExample';
-import { PaddingXExample } from './PaddingXExample';
-import { PaddingYExample } from './PaddingYExample';
 import { PlacementExample } from './PlacementExample';
 
 const Page = () => (
@@ -29,21 +14,6 @@ const Page = () => (
     <OrientationExample />
     <PlacementExample />
     <PaddingExample />
-    <PaddingPExample />
-    <PaddingXExample />
-    <PaddingYExample />
-    <PaddingTExample />
-    <PaddingBExample />
-    <PaddingLExample />
-    <PaddingRExample />
-    <MarginExample />
-    <MarginMExample />
-    <MarginXExample />
-    <MarginYExample />
-    <MarginTExample />
-    <MarginBExample />
-    <MarginLExample />
-    <MarginRExample />
     <FontExample />
     <ColorExample />
     <GapExample />
