15147e4b4ee23bc838512f3ad41a971036c91828
Richard Wagner <richard.wagner.sd@gmail.com>
2025-07-17 19:50:14 +0200

    fix: delete

--- src/core/utils/merge-class-name/style-avatar-group.ts
+++
@@ -1,8 +0,0 @@
-import { systemStyles } from '../../config/system-styles';
-import type { Classes } from '../../types';
-import { mergeClasses } from './merge-classes';
-
-export const styleAvataGroup = (
-  className: string,
-  { orientation = 'row', spacing = 'unset' }: Classes
-) => mergeClasses(systemStyles.spacing[orientation][spacing], className);
