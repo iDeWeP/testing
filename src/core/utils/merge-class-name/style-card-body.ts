ec623c4dbd29871ad87a8e8b6debfddc75defaef
Richard Wagner <richard.wagner.sd@gmail.com>
2025-07-17 23:25:30 +0200

    fix: card

---
+++ src/core/utils/merge-class-name/style-card-body.ts
@@ -0,0 +1,44 @@
+import { sharedStyles } from '../../config/shared-styles';
+import { systemStyles } from '../../config/system-styles';
+import type { Classes } from '../../types';
+import { getBorder } from './border';
+import { mergeClasses } from './merge-classes';
+import { getSpacing } from './spacing';
+
+export const styleCardBody = (
+  className: string,
+  {
+    variant = 'solid',
+    orientation = 'row',
+    cardPlacement = 'none',
+    padding = 'unset',
+    margin = 'unset',
+    border = 'unset',
+    radius = 'unset'
+  }: Classes
+) => {
+  const paddings = getSpacing(padding, cardPlacement);
+  const margins = getSpacing(margin, cardPlacement);
+  const borders = getBorder(variant, border, cardPlacement);
+
+  return mergeClasses(
+    sharedStyles.display.flex,
+    systemStyles.padding.default.all[paddings.all],
+    systemStyles.padding.default.x[paddings.x],
+    systemStyles.padding.default.y[paddings.y],
+    systemStyles.padding.default.t[paddings.t],
+    systemStyles.padding.default.b[paddings.b],
+    systemStyles.margin.all[margins.all],
+    systemStyles.margin.x[margins.x],
+    systemStyles.margin.y[margins.y],
+    systemStyles.margin.t[margins.t],
+    systemStyles.margin.b[margins.b],
+    systemStyles.border.all[borders.all],
+    systemStyles.border.x[borders.x],
+    systemStyles.border.t[borders.t],
+    systemStyles.border.b[borders.b],
+    systemStyles.radius.b[radius],
+    sharedStyles.direction[orientation],
+    className
+  );
+};
