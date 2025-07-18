fac2c28df90734fdcb187378a8f7c949859bb7f9
Richard Wagner <richard.wagner.sd@gmail.com>
2025-07-18 16:55:39 +0200

    fix: avatr

--- src/core/components/AvatarGroup/AvatarGroup.types.ts
+++ src/core/components/AvatarGroup/AvatarGroup.types.ts
@@ -1,51 +1,15 @@
 import type { ElementType } from 'react';
-import type {
-  Variant,
-  Size,
-  Border,
-  DefaultBorder,
-  Radius,
-  Color,
-  Shadow,
-  ComponentRefProps,
-  Orientation,
-  Spacing,
-  Gap,
-  MergeProps
-} from '../../types';
+import type { Orientation, Spacing, Gap, MergeProps } from '../../types';
+import type { AvatarProps } from '../Avatar/Avatar.types';
 import type {
   UnstyledGroupConfigProps,
   UnstyledGroupProps
 } from '../UnstyledGroup/UnstyledGroup.types';
 
-export type AvatarGroupsurplusProps = {
-  as?: ElementType;
-  variant?: Variant;
-  size?: Size;
-  border?: Border;
-  b?: DefaultBorder;
-  bx?: DefaultBorder;
-  by?: DefaultBorder;
-  bt?: DefaultBorder;
-  bb?: DefaultBorder;
-  bl?: DefaultBorder;
-  br?: DefaultBorder;
-  radius?: Radius;
-  r?: Radius;
-  rt?: Radius;
-  rb?: Radius;
-  rl?: Radius;
-  rr?: Radius;
-  rtl?: Radius;
-  rtr?: Radius;
-  rbl?: Radius;
-  rbr?: Radius;
-  color?: Color;
-  shadow?: Shadow;
-} & ComponentRefProps<'div'>;
+export type AvatarGroupSurplusProps = Omit<AvatarProps<'div'>, 'as'>;
 
 export type AvatarGroupComponentsProps = {
-  surplus?: AvatarGroupsurplusProps;
+  surplus?: AvatarGroupSurplusProps;
 };
 
 export type AvatarGroupOptionalProps = {
