527846f43a68b5677d47f36a842e4c544970c853
Richard Wagner <richard.wagner.sd@gmail.com>
2025-07-18 17:18:21 +0200

    fix; abvatar

--- src/core/components/AvatarGroup/AvatarGroup.types.ts
+++ src/core/components/AvatarGroup/AvatarGroup.types.ts
@@ -1,12 +1,60 @@
 import type { ElementType } from 'react';
-import type { Orientation, Spacing, Gap, MergeProps } from '../../types';
-import type { AvatarProps } from '../Avatar/Avatar.types';
+import type {
+  Variant,
+  Size,
+  Scale,
+  Margin,
+  Border,
+  DefaultBorder,
+  Radius,
+  Color,
+  Shadow,
+  Gap,
+  Orientation,
+  Spacing,
+  MergeProps
+} from '../../types';
 import type {
   UnstyledGroupConfigProps,
   UnstyledGroupProps
 } from '../UnstyledGroup/UnstyledGroup.types';
 
-export type AvatarGroupSurplusProps = Omit<AvatarProps<'div'>, 'as'>;
+export type AvatarGroupSurplusProps = {
+  initials?: boolean;
+  variant?: Variant;
+  size?: Size;
+  scale?: Scale;
+  margin?: Margin;
+  m?: Margin;
+  mx?: Margin;
+  my?: Margin;
+  mt?: Margin;
+  mb?: Margin;
+  ml?: Margin;
+  mr?: Margin;
+  border?: Border;
+  b?: DefaultBorder;
+  bx?: DefaultBorder;
+  by?: DefaultBorder;
+  bt?: DefaultBorder;
+  bb?: DefaultBorder;
+  bl?: DefaultBorder;
+  br?: DefaultBorder;
+  radius?: Radius;
+  r?: Radius;
+  rt?: Radius;
+  rb?: Radius;
+  rl?: Radius;
+  rr?: Radius;
+  rtl?: Radius;
+  rtr?: Radius;
+  rbl?: Radius;
+  rbr?: Radius;
+  color?: Color;
+  ring?: Color;
+  shadow?: Shadow;
+  gap?: Gap;
+};
 
 export type AvatarGroupComponentsProps = {
   surplus?: AvatarGroupSurplusProps;
