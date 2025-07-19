ee8cd958dabd5dc6c05c16ee63f90249d7eb9b93
Richard Wagner <richard.wagner.sd@gmail.com>
2025-07-19 17:47:54 +0200

    fix: anhor

--- src/core/components/Anchor/anchorConfig.ts
+++ src/core/components/Anchor/anchorConfig.ts
@@ -42,6 +42,7 @@ export const anchorConfig: ComponentConfig<AnchorConfigProps> = {
     rbl: 'unset',
     rbr: 'unset',
     shadow: 'unset',
+    ring: 'unset',
     gap: 'unset',
     gx: 'unset',
     gy: 'unset'
