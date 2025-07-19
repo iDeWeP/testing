f4e8d441e56a403c00fdf44ee193412a20bba4f6
Richard Wagner <richard.wagner.sd@gmail.com>
2025-07-19 19:24:04 +0200

    fix; dropdown

--- src/routes/core/unstyled-dropdown/Page.tsx
+++ src/routes/core/unstyled-dropdown/Page.tsx
@@ -32,6 +32,7 @@ import { RadiusRRExample } from './RadiusRRExample';
 import { RadiusRTExample } from './RadiusRTExample';
 import { RadiusRTLExample } from './RadiusRTLExample';
 import { RadiusRTRExample } from './RadiusRTRExample';
+import { RingExample } from './RingExample';
 import { ShadowExample } from './ShadowExample';
 import { VariantExample } from './VariantExample';
 
@@ -70,6 +71,7 @@ const Page = () => (
     <FontExample />
     <ColorExample />
     <ShadowExample />
+    <RingExample />
     <GapExample />
     <GapXExample />
     <GapYExample />
