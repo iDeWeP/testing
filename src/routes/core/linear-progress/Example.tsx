ac24641111d11700baa37f82aa1ac29e48f10a8c
Richard Wagner <richard.wagner.sd@gmail.com>
2025-07-19 13:32:01 +0200

    ix

--- src/routes/core/linear-progress/Example.tsx
+++ src/routes/core/linear-progress/Example.tsx
@@ -2,5 +2,8 @@ import { LinearProgress } from '../../../core/components/LinearProgress/LinearPr
 import type { LinearProgressProps } from '../../../core/components/LinearProgress/LinearProgress.types';
 
 export const Example = (props: LinearProgressProps) => (
-  <LinearProgress {...props} />
+  <LinearProgress
+    value={75}
+    {...props}
+  />
 );
