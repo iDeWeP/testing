d0cbe8e7e8713222024803f16868382f967f5ef3
Richard Wagner <richard.wagner.sd@gmail.com>
2025-07-19 18:59:37 +0200

    fix; card

--- src/routes/core/unstyled-card/Page.tsx
+++ src/routes/core/unstyled-card/Page.tsx
@@ -40,6 +40,7 @@ import { RadiusRRExample } from './RadiusRRExample';
 import { RadiusRTExample } from './RadiusRTExample';
 import { RadiusRTLExample } from './RadiusRTLExample';
 import { RadiusRTRExample } from './RadiusRTRExample';
+import { RingExample } from './RingExample';
 import { ShadowExample } from './ShadowExample';
 import { VariantExample } from './VariantExample';
 
@@ -86,6 +87,7 @@ const Page = () => (
     <FontExample />
     <ColorExample />
     <ShadowExample />
+    <RingExample />
     <GapExample />
     <GapXExample />
     <GapYExample />
