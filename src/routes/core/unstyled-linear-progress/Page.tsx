edcede892d6e21562acef020fe96f940c457706d
Richard Wagner <richard.wagner.sd@gmail.com>
2025-07-20 00:56:18 +0200

    fix: linear

--- src/routes/core/unstyled-linear-progress/Page.tsx
+++ src/routes/core/unstyled-linear-progress/Page.tsx
@@ -30,6 +30,7 @@ import { RadiusRRExample } from './RadiusRRExample';
 import { RadiusRTExample } from './RadiusRTExample';
 import { RadiusRTLExample } from './RadiusRTLExample';
 import { RadiusRTRExample } from './RadiusRTRExample';
+import { RingExample } from './RingExample';
 import { ShadowExample } from './ShadowExample';
 import { SizeExample } from './SizeExample';
 import { ValueExample } from './ValueExample';
@@ -72,6 +73,7 @@ const Page = () => (
     <RadiusRBRExample />
     <ColorExample />
     <ShadowExample />
+    <RingExample />
     <GapExample />
   </>
 );
