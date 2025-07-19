8ac9004eec378911633352e2c79364a46afa597a
Richard Wagner <richard.wagner.sd@gmail.com>
2025-07-19 18:11:50 +0200

    fix: footer

--- src/core/components/Footer/footerConfig.ts
+++ src/core/components/Footer/footerConfig.ts
@@ -44,6 +44,7 @@ export const footerConfig: ComponentConfig<FooterConfigProps> = {
     font: 'unset',
     color: 'unset',
     shadow: 'unset',
+    ring: 'unset',
     gx: 'unset',
     gy: 'unset'
   }
