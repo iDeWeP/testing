d0d8488f7ec78808458bf72a52b830eb9a20c757
Richard Wagner <richard.wagner.sd@gmail.com>
2025-07-20 16:41:16 +0200

    fix: close-button

--- src/core/components/UnstyledCloseButton/UnstyledCloseButton.types.ts
+++ src/core/components/UnstyledCloseButton/UnstyledCloseButton.types.ts
@@ -12,33 +12,24 @@ import type {
 } from '../../types';
 import type {
   UnstyledButtonRippleProps,
-  UnstyledButtonSpinnerProps,
+  UnstyledButtonLoaderProps,
   UnstyledButtonBarProps,
-  UnstyledButtonTrailProps,
-  UnstyledButtonInnerBorderProps,
-  UnstyledButtonOuterBorderProps,
   UnstyledButtonConfigProps,
   UnstyledButtonProps
 } from '../UnstyledButton/UnstyledButton.types';
 
 export type UnstyledCloseButtonIconProps = ComponentRefProps<'svg'>;
 export type UnstyledCloseButtonRippleProps = UnstyledButtonRippleProps;
-export type UnstyledCloseButtonSpinnerProps = UnstyledButtonSpinnerProps;
+export type UnstyledCloseButtonLoaderProps = UnstyledButtonLoaderProps;
 export type UnstyledCloseButtonBarProps = UnstyledButtonBarProps;
-export type UnstyledCloseButtonTrailProps = UnstyledButtonTrailProps;
-export type UnstyledCloseButtonInnerBorderProps =
-  UnstyledButtonInnerBorderProps;
-export type UnstyledCloseButtonOuterBorderProps =
-  UnstyledButtonOuterBorderProps;
 
 export type UnstyledCloseButtonComponentsProps = {
   icon?: UnstyledCloseButtonIconProps;
   ripple?: UnstyledCloseButtonRippleProps;
-  spinner?: UnstyledCloseButtonSpinnerProps;
+  loader?: UnstyledCloseButtonLoaderProps;
   bar?: UnstyledCloseButtonBarProps;
-  trail?: UnstyledCloseButtonTrailProps;
-  innerBorder?: UnstyledCloseButtonInnerBorderProps;
-  outerBorder?: UnstyledCloseButtonOuterBorderProps;
+  innerBorder?: UnstyledCloseButtonBarProps;
+  outerBorder?: UnstyledCloseButtonBarProps;
 };
 
 export type UnstyledCloseButtonOptionalProps = {
