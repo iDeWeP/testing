0e05b1a50679633773d14762605ae2c343c46079
Richard Wagner <richard.wagner.sd@gmail.com>
2025-07-20 16:49:34 +0200

    fix: button

--- src/core/utils/merge-class-name/style-button.ts
+++ src/core/utils/merge-class-name/style-button.ts
@@ -2,9 +2,10 @@ import { unstyledButtonConfig } from '../../components/UnstyledButton/unstyledBu
 import { sharedStyles } from '../../config/shared-styles';
 import { systemStyles } from '../../config/system-styles';
 import type { Classes } from '../../types';
-import { getColor, getCheckableColor } from './color';
+import { getColor } from './get-color';
+import { getFocusableState } from './get-focusable-state';
+import { getStateColor } from './get-state-color';
 import { mergeClasses } from './merge-classes';
-import { getFocusableState, getLoadingState } from './state';
 
 export const styleButton = (
   className: string,
@@ -18,17 +19,32 @@ export const styleButton = (
     effect = 'unset'
   }: Classes
 ) => {
-  const colors = getColor(variant, getCheckableColor(color, checked), disabled);
+  const state = !loading ? 'none' : loading === true ? 'hide' : 'normal';
+  const isCheckable = checked !== undefined && !disabled;
+  const defaultColors = getColor(variant, color, { disabled });
+  const uncheckedColors = getColor(variant, color, { disabled }, false);
+  const checkedColors = getColor(variant, color, { disabled }, true);
+  const colors = getStateColor(
+    defaultColors,
+    uncheckedColors,
+    checkedColors,
+    disabled,
+    checked
+  );
 
   return mergeClasses(
     checked && unstyledButtonConfig.styles.root.checked,
     sharedStyles.position.relative,
     sharedStyles.focusable[getFocusableState(disabled, loading)],
-    disabled && systemStyles.color.bg[theme][colors.bg],
-    disabled && systemStyles.color.text[theme][colors.text],
-    disabled && systemStyles.color.fill[theme][colors.text],
-    systemStyles.color.ring[theme][colors.bg],
-    unstyledButtonConfig.styles.root.loading[getLoadingState(loading)],
+    disabled && systemStyles.color.normal.bg[theme][colors.bg],
+    disabled && systemStyles.color.normal.text[theme][colors.text],
+    disabled && systemStyles.color.normal.fill[theme][colors.text],
+    systemStyles.color.normal.ring[theme][colors.ring],
+    isCheckable && systemStyles.color.checked.bg[theme][checkedColors.bg],
+    isCheckable && systemStyles.color.checked.text[theme][checkedColors.text],
+    isCheckable && systemStyles.color.checked.fill[theme][checkedColors.text],
+    isCheckable && systemStyles.color.checked.ring[theme][checkedColors.ring],
+    unstyledButtonConfig.styles.root.loading[state],
     sharedStyles.effect[effect],
     className
   );
