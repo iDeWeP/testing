61738f7fa32390e156331390521da9666febbb8e
Richard Wagner <richard.wagner.sd@gmail.com>
2025-07-21 08:04:35 +0200

    fix

--- src/core/utils/is-auto-bordered/is-auto-bordered.ts
+++ src/core/utils/is-auto-bordered/is-auto-bordered.ts
@@ -1,4 +1,4 @@
 import type { Variant, Border } from '../../types';
 
-export const isBordered = (variant: Variant, border: Border) =>
+export const isAutoBordered = (variant: Variant, border: Border) =>
   (variant === 'outlined' && border === 'auto') || border === 'set';
