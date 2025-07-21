79c64e0d2da4ac78cea626fbde09c5c32bdd0d17
Richard Wagner <richard.wagner.sd@gmail.com>
2025-07-21 21:55:26 +0200

    fix: avatar

--- src/core/components/AvatarGroup/AvatarGroup.types.ts
+++ src/core/components/AvatarGroup/AvatarGroup.types.ts
@@ -5,7 +5,6 @@ import type {
   Size,
   Scale,
   Border,
-  DefaultBorder,
   Radius,
   Color,
   Shadow,
@@ -26,13 +25,6 @@ export type AvatarGroupSurplusProps = MergeProps<
     size?: Size;
     scale?: Scale;
     border?: Border;
-    b?: DefaultBorder;
-    bx?: DefaultBorder;
-    by?: DefaultBorder;
-    bt?: DefaultBorder;
-    bb?: DefaultBorder;
-    bl?: DefaultBorder;
-    br?: DefaultBorder;
     radius?: Radius;
     r?: Radius;
     rt?: Radius;
