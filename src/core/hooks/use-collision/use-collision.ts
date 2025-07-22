39974df2454bc2ab8f9efc6bc85c23f3b3859226
Richard Wagner <richard.wagner.sd@gmail.com>
2025-07-22 17:36:08 +0200

    fix: hooks

--- src/core/hooks/use-collision/use-collision.ts
+++ src/core/hooks/use-collision/use-collision.ts
@@ -1,9 +1,11 @@
 import { useEffect } from 'react';
 import type { Placement } from '../../types';
 
+export type CollisionFunction = (placement: Placement) => void;
+
 export const useCollision = (
   placement: Placement,
-  onCollision?: (placement: Placement) => void
+  onCollision?: CollisionFunction
 ): void =>
   useEffect(() => {
     if (onCollision) {
