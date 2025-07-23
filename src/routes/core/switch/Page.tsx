bd2a97f58e5688e5e621bced8f879b2e9d2c42b8
Richard Wagner <richard.wagner.sd@gmail.com>
2025-07-23 20:09:44 +0200

    fix: switch

--- src/routes/core/switch/Page.tsx
+++ src/routes/core/switch/Page.tsx
@@ -3,15 +3,19 @@ import { ExampleTitle } from '../../../components/ExampleTitle';
 import { ColorExample } from './ColorExample';
 import { DefaultExample } from './DefaultExample';
 import { DisabledExample } from './DisabledExample';
+import { InvalidExample } from './InvalidExample';
 import { RippleExample } from './RippleExample';
 import { ScaleExample } from './ScaleExample';
 import { SizeExample } from './SizeExample';
+import { ValidExample } from './ValidExample';
 import { VariantExample } from './VariantExample';
 
 const Page = (): ReactNode => (
   <>
     <ExampleTitle>Switch</ExampleTitle>
     <DefaultExample />
+    <ValidExample />
+    <InvalidExample />
     <DisabledExample />
     <VariantExample />
     <SizeExample />
