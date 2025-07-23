84f9c89a75de46455e07e618b924d0e4e67add82
Richard Wagner <richard.wagner.sd@gmail.com>
2025-07-23 23:24:33 +0200

    fixl button

--- src/routes/core/unstyled-button/Page.tsx
+++ src/routes/core/unstyled-button/Page.tsx
@@ -1,7 +1,6 @@
 import type { ReactNode } from 'react';
 import { ExampleTitle } from '../../../components/ExampleTitle';
 import { BorderExample } from './BorderExample';
-import { CheckedExample } from './CheckedExample';
 import { ColorExample } from './ColorExample';
 import { DefaultExample } from './DefaultExample';
 import { DisabledExample } from './DisabledExample';
@@ -35,7 +34,6 @@ const Page = (): ReactNode => (
   <>
     <ExampleTitle>UnstyledButton</ExampleTitle>
     <DefaultExample />
-    <CheckedExample />
     <LoadingExample />
     <DisabledExample />
     <VariantExample />
