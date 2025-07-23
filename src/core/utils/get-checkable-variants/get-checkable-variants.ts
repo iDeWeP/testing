315274a19c49e3c74509d5c3cfa989a83e28d941
Richard Wagner <richard.wagner.sd@gmail.com>
2025-07-23 20:34:33 +0200

    fix: variant

---
+++ src/core/utils/get-checkable-variants/get-checkable-variants.ts
@@ -0,0 +1,11 @@
+import type { CheckableVariant, Variant } from '../../types';
+
+export const getCheckableVariants = (
+  variant: CheckableVariant
+): [Variant, Variant] => {
+  const variants = variant.split('-') as Variant[];
+
+  return variants.length === 1
+    ? ['outlined', variants[0]]
+    : [variants[0], variants[1]];
+};
