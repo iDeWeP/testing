4fd789760a5aa477e6aeab30ad5bbbb4481b0bec
Richard Wagner <richard.wagner.sd@gmail.com>
2025-07-23 20:05:29 +0200

    fix: checkbox

--- src/routes/core/unstyled-switch/VariantExample.tsx
+++ src/routes/core/unstyled-switch/VariantExample.tsx
@@ -11,5 +11,29 @@ export const VariantExample = (): ReactNode => (
       <Example variant="light" />
       <Example variant="solid" />
     </ExampleCard>
+    <ExampleCard>
+      <Example variant="text-text" />
+      <Example variant="text-outlined" />
+      <Example variant="text-light" />
+      <Example variant="text-solid" />
+    </ExampleCard>
+    <ExampleCard>
+      <Example variant="outlined-text" />
+      <Example variant="outlined-outlined" />
+      <Example variant="outlined-light" />
+      <Example variant="outlined-solid" />
+    </ExampleCard>
+    <ExampleCard>
+      <Example variant="light-text" />
+      <Example variant="light-outlined" />
+      <Example variant="light-light" />
+      <Example variant="light-solid" />
+    </ExampleCard>
+    <ExampleCard>
+      <Example variant="solid-text" />
+      <Example variant="solid-outlined" />
+      <Example variant="solid-light" />
+      <Example variant="solid-solid" />
+    </ExampleCard>
   </ExampleSection>
 );
