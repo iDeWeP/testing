c8bad5e399a7fd05ef77eb0a6dce5d67c9b0d5f5
Richard Wagner <richard.wagner.sd@gmail.com>
2025-07-24 09:24:31 +0200

    fix; group

--- src/core/components/Group/groupConfig.ts
+++ src/core/components/Group/groupConfig.ts
@@ -44,8 +44,6 @@ export const groupConfig: ComponentConfig<GroupConfigProps> = {
     font: 'unset',
     color: 'unset',
     shadow: 'unset',
-    ring: 'unset',
-    gx: 'unset',
-    gy: 'unset'
+    ring: 'unset'
   }
 };
