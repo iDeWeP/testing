6cfe145906b0773208d90ed69ac43d74c2875eb4
Richard Wagner <richard.wagner.sd@gmail.com>
2025-07-24 19:01:13 +0200

    fix: container

--- src/routes/core/unstyled-container/index.tsx
+++ src/routes/core/unstyled-container/index.tsx
@@ -3,5 +3,9 @@ import type { RouteObject } from 'react-router';
 
 export const UnstyledContainerRoute: RouteObject = {
   path: '/core/unstyled-container',
-  Component: lazy(() => import('./Page'))
+  Component: lazy(
+    (): Promise<
+      typeof import('/Users/ritchiw/vscode/testing/src/routes/core/unstyled-container/Page')
+    > => import('./Page')
+  )
 };
