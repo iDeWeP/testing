2a3da7d80571933fe0c825e872c09056c52ed433
Richard Wagner <richard.wagner.sd@gmail.com>
2025-07-28 20:07:08 +0200

    fix: toggle-button

---
+++ src/core/utils/merge-class-name/style-toggle-button.ts
@@ -0,0 +1,33 @@
+import { sharedStyle } from '../../config/shared-style';
+import { systemStyle } from '../../config/system-style';
+import type { Classes, ClassName } from '../../types';
+import { getColor } from './get-color';
+import { getFocusableState } from './get-focusable-state';
+import { mergeClasses } from './merge-classes';
+
+export const styleToggleButton = (
+  className: string,
+  {
+    theme = 'light',
+    checked = false,
+    disabled = false,
+    variant = 'solid',
+    color = 'unset',
+    effect = 'unset'
+  }: Classes
+): ClassName => {
+  const focusable = getFocusableState({ disabled });
+  const autoColor = getColor(variant, color, { checked, disabled });
+
+  return mergeClasses(
+    checked && sharedStyle.state.on,
+    sharedStyle.button,
+    sharedStyle.focusable[focusable],
+    disabled && systemStyle.color.default.bg[theme][autoColor.bg],
+    disabled && systemStyle.color.default.text[theme][autoColor.text],
+    disabled && systemStyle.color.default.fill[theme][autoColor.text],
+    systemStyle.color.default.ring[theme][autoColor.ring],
+    sharedStyle.effect[effect],
+    className
+  );
+};
