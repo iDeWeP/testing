eb28e445e779dcbc20822ebd38d639581f4c70fd
Richard Wagner <richard.wagner.sd@gmail.com>
2025-07-28 14:34:16 +0200

    fix: layout

--- src/core/components/UnstyledLayout/UnstyledLayout.types.ts
+++ src/core/components/UnstyledLayout/UnstyledLayout.types.ts
@@ -1,9 +1,20 @@
 import type { ElementType } from 'react';
+import type { Layout, MergeProps } from '../../types';
 import type {
   UnstyledBoxConfigProps,
   UnstyledBoxProps
 } from '../UnstyledBox/UnstyledBox.types';
 
-export type UnstyledLayoutConfigProps = UnstyledBoxConfigProps;
+type DefaultProps = {
+  layout?: Layout;
+};
 
-export type UnstyledLayoutProps<E extends ElementType> = UnstyledBoxProps<E>;
+export type UnstyledLayoutConfigProps = MergeProps<
+  UnstyledBoxConfigProps,
+  DefaultProps
+>;
+
+export type UnstyledLayoutProps<E extends ElementType> = MergeProps<
+  UnstyledBoxProps<E>,
+  DefaultProps
+>;
