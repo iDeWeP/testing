767a7a688a3e9de2b7ba8b598628e44199d18071
Richard Wagner <richard.wagner.sd@gmail.com>
2025-07-28 18:45:35 +0200

    fix: button

--- src/routes/core/unstyled-toggle-button/DefaultExample.tsx
+++ src/routes/core/unstyled-toggle-button/DefaultExample.tsx
@@ -7,7 +7,6 @@ export const DefaultExample = (): ReactNode => (
   <ExampleSection title="Default">
     <ExampleCard>
       <Example clear />
-      <Example ripple="none">RIPPLE-NONE</Example>
     </ExampleCard>
   </ExampleSection>
 );
