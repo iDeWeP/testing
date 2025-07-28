ff4e57fa0c50e07f195d9dd17c540f7ae04ae245
Richard Wagner <richard.wagner.sd@gmail.com>
2025-07-28 16:16:42 +0200

    fix: label

--- src/routes/core/unstyled-circular-progress/DefaultExample.tsx
+++ src/routes/core/unstyled-circular-progress/DefaultExample.tsx
@@ -7,6 +7,7 @@ export const DefaultExample = (): ReactNode => (
   <ExampleSection title="Default">
     <ExampleCard>
       <Example clear />
+      <Example>LABEL</Example>
     </ExampleCard>
   </ExampleSection>
 );
