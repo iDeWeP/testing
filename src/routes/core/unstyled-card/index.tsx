d553349fa1ef41f451a015d662a26734b6484711
Richard Wagner <richard.wagner.sd@gmail.com>
2025-07-28 07:19:53 +0200

    fix: card

--- src/routes/core/unstyled-card/index.tsx
+++ src/routes/core/unstyled-card/index.tsx
@@ -1,11 +1,11 @@
 import { lazy } from 'react';
 import type { RouteObject } from 'react-router';
 
-export const UnstyledBoxRoute: RouteObject = {
-  path: '/core/unstyled-box',
+export const UnstyledCardRoute: RouteObject = {
+  path: '/core/unstyled-card',
   Component: lazy(
     (): Promise<
-      typeof import('/Users/ritchiw/vscode/testing/src/routes/core/unstyled-box/Page')
+      typeof import('/Users/ritchiw/vscode/testing/src/routes/core/unstyled-card/Page')
     > => import('./Page')
   )
 };
