dd27e0491925dff8a5c4ce2847ddd0062c5bf4be
Richard Wagner <richard.wagner.sd@gmail.com>
2025-07-29 23:34:06 +0200

    fix: anchor

--- src/routes/core/anchor/Page.tsx
+++ src/routes/core/anchor/Page.tsx
@@ -1,15 +1,11 @@
 import type { ReactNode } from 'react';
 import { ExampleTitle } from '../../../components/ExampleTitle';
-import { ColorExample } from './ColorExample';
 import { DefaultExample } from './DefaultExample';
-import { FontExample } from './FontExample';
 
 const Page = (): ReactNode => (
   <>
     <ExampleTitle>Anchor</ExampleTitle>
     <DefaultExample />
-    <FontExample />
-    <ColorExample />
   </>
 );
 
