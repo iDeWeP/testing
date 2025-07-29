0318f8303506cd29598ce60b615de244fb2542de
Richard Wagner <richard.wagner.sd@gmail.com>
2025-07-29 23:35:19 +0200

    fix: avatar

--- src/routes/core/avatar/Page.tsx
+++ src/routes/core/avatar/Page.tsx
@@ -1,19 +1,11 @@
 import type { ReactNode } from 'react';
 import { ExampleTitle } from '../../../components/ExampleTitle';
-import { ColorExample } from './ColorExample';
 import { DefaultExample } from './DefaultExample';
-import { InitialsExample } from './InitialsExample';
-import { SizeExample } from './SizeExample';
-import { VariantExample } from './VariantExample';
 
 const Page = (): ReactNode => (
   <>
     <ExampleTitle>Avatar</ExampleTitle>
     <DefaultExample />
-    <InitialsExample />
-    <VariantExample />
-    <SizeExample />
-    <ColorExample />
   </>
 );
 
