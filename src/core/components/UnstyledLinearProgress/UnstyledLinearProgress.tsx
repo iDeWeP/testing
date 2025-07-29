db8d3966c2ea9aaca2c28d01326c7e580012a902
Richard Wagner <richard.wagner.sd@gmail.com>
2025-07-29 05:39:43 +0200

    fix: linear

--- src/core/components/UnstyledLinearProgress/UnstyledLinearProgress.tsx
+++ src/core/components/UnstyledLinearProgress/UnstyledLinearProgress.tsx
@@ -82,7 +82,7 @@ export const UnstyledLinearProgress = (
   return (
     <div
       className={mergedClassName}
-      {...setAria('linearProgress', { min: 0, max: 100, value })}
+      {...setAria('progress', { min: 0, max: 100, value })}
       {...restProps}
     >
       <UnstyledLinearProgressBar
