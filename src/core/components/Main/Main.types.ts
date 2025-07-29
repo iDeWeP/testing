d770116548641abba14dbe1a32312b2bd9b1f194
Richard Wagner <richard.wagner.sd@gmail.com>
2025-07-29 19:36:26 +0200

    fix: main

--- src/core/components/Main/Main.types.ts
+++ src/core/components/Main/Main.types.ts
@@ -5,17 +5,17 @@ import type {
   UnstyledPaperProps
 } from '../UnstyledPaper/UnstyledPaper.types';
 
-export type MainDefaultProps = {
+type DefaultProps = {
   orientation?: Orientation;
   gap?: Gap;
 };
 
 export type MainConfigProps = MergeProps<
   UnstyledPaperConfigProps,
-  MainDefaultProps
+  DefaultProps
 >;
 
 export type MainProps<E extends ElementType> = MergeProps<
   UnstyledPaperProps<E>,
-  MainDefaultProps
+  DefaultProps
 >;
--- src/core/components/Main/mainConfig.ts
+++ src/core/components/Main/mainConfig.ts
@@ -44,8 +44,6 @@ export const mainConfig: ComponentConfig<MainConfigProps> = {
     font: 'unset',
     color: 'unset',
     shadow: 'unset',
-    ring: 'unset',
-    gx: 'unset',
-    gy: 'unset'
+    ring: 'unset'
   }
 };
