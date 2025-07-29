142191bb75ea837c0b9c239f5fbb3b0630c27e5f
Richard Wagner <richard.wagner.sd@gmail.com>
2025-07-29 13:31:58 +0200

    fix: checkbale

--- src/core/utils/get-checkable-variants/get-checkable-variants.ts
+++ src/core/utils/get-checkable-variants/get-checkable-variants.ts
@@ -1,11 +1,12 @@
 import type { CheckableVariant, Variant } from '../../types';
 
 export const getCheckableVariants = (
-  variant: CheckableVariant
+  variant: CheckableVariant,
+  defaultVariant?: Variant
 ): [Variant, Variant] => {
   const variants = variant.split('-') as Variant[];
 
   return variants.length === 1
-    ? ['outlined', variants[0]]
+    ? [defaultVariant || 'outlined', variants[0]]
     : [variants[0], variants[1]];
 };
