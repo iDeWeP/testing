2ffa94293962f9395c5bf44a2a56a30115ed9dad
Richard Wagner <richard.wagner.sd@gmail.com>
2025-07-29 20:34:53 +0200

    fix; typo

--- src/core/components/UnstyledTypography/UnstyledTypography.types.ts
+++ src/core/components/UnstyledTypography/UnstyledTypography.types.ts
@@ -1,17 +1,12 @@
 import type { ElementType } from 'react';
-import type { MergeProps, OmitGaps } from '../../types';
-import type { DefaultProps } from '../CircularProgress/CircularProgress.types';
+import type { OmitGaps } from '../../types';
 import type {
   UnstyledBoxConfigProps,
   UnstyledBoxProps
 } from '../UnstyledBox/UnstyledBox.types';
 
-export type UnstyledTypographyConfigProps = MergeProps<
-  OmitGaps<UnstyledBoxConfigProps>,
-  DefaultProps
->;
+export type UnstyledTypographyConfigProps = OmitGaps<UnstyledBoxConfigProps>;
 
-export type UnstyledTypographyProps<E extends ElementType> = MergeProps<
-  OmitGaps<UnstyledBoxProps<E>>,
-  DefaultProps
+export type UnstyledTypographyProps<E extends ElementType> = OmitGaps<
+  UnstyledBoxProps<E>
 >;
