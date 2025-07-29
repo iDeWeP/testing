900dde17289f9a8edf737150531833a8cd53287f
Richard Wagner <richard.wagner.sd@gmail.com>
2025-07-29 15:55:23 +0200

    fx :conatinar

--- src/core/components/Container/Container.types.ts
+++ src/core/components/Container/Container.types.ts
@@ -1,31 +1,9 @@
 import type { ElementType } from 'react';
 import type {
-  Variant,
-  Size,
-  Scale,
-  Radius,
-  Color,
-  MergeProps
-} from '../../types';
-import type {
   UnstyledContainerConfigProps,
   UnstyledContainerProps
 } from '../UnstyledContainer/UnstyledContainer.types';
 
-export type ContainerDefaultProps = {
-  variant?: Variant;
-  size?: Size;
-  scale?: Scale;
-  radius?: Radius;
-  color?: Color;
-};
+export type ContainerConfigProps = UnstyledContainerConfigProps;
 
-export type ContainerConfigProps = MergeProps<
-  UnstyledContainerConfigProps,
-  ContainerDefaultProps
->;
-
-export type ContainerProps<E extends ElementType> = MergeProps<
-  UnstyledContainerProps<E>,
-  ContainerDefaultProps
->;
+export type ContainerProps<E extends ElementType> = UnstyledContainerProps<E>;
