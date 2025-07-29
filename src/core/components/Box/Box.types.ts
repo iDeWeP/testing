fd9269eea03c966b240b0444952829f6059a636b
Richard Wagner <richard.wagner.sd@gmail.com>
2025-07-29 14:59:06 +0200

    fix: box

--- src/core/components/Box/Box.types.ts
+++ src/core/components/Box/Box.types.ts
@@ -1,16 +1,9 @@
 import type { ElementType } from 'react';
-import type { MergeProps } from '../../types';
 import type {
-  UnstyledBoxDefaultProps,
   UnstyledBoxConfigProps,
   UnstyledBoxProps
 } from '../UnstyledBox/UnstyledBox.types';
 
-export type BoxDefaultProps = UnstyledBoxDefaultProps;
-
 export type BoxConfigProps = UnstyledBoxConfigProps;
 
-export type BoxProps<E extends ElementType> = MergeProps<
-  UnstyledBoxProps<E>,
-  BoxDefaultProps
->;
+export type BoxProps<E extends ElementType> = UnstyledBoxProps<E>;
