fdf16c64140719ac2d55d36a4240c2acf9089ab8
Richard Wagner <richard.wagner.sd@gmail.com>
2025-07-29 19:47:02 +0200

    fix; section

--- src/core/components/Section/Section.types.ts
+++ src/core/components/Section/Section.types.ts
@@ -5,17 +5,17 @@ import type {
   UnstyledPaperProps
 } from '../UnstyledPaper/UnstyledPaper.types';
 
-export type SectionDefaultProps = {
+type DefaultProps = {
   orientation?: Orientation;
   gap?: Gap;
 };
 
 export type SectionConfigProps = MergeProps<
   UnstyledPaperConfigProps,
-  SectionDefaultProps
+  DefaultProps
 >;
 
 export type SectionProps<E extends ElementType> = MergeProps<
   UnstyledPaperProps<E>,
-  SectionDefaultProps
+  DefaultProps
 >;
