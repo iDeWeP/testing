a22fd9e1c20c1bf1bb8d4ef6e90cd8f73f671644
Richard Wagner <richard.wagner.sd@gmail.com>
2025-07-30 09:35:14 +0200

    fix: mark

--- src/routes/core/mark/index.tsx
+++ src/routes/core/mark/index.tsx
@@ -3,5 +3,9 @@ import type { RouteObject } from 'react-router';
 
 export const MarkRoute: RouteObject = {
   path: '/core/mark',
-  Component: lazy(() => import('./Page'))
+  Component: lazy(
+    (): Promise<
+      typeof import('/Users/ritchiw/vscode/testing/src/routes/core/mark/Page')
+    > => import('./Page')
+  )
 };
