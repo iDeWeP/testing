5448ceefa4737d8d0ea71240e68d6cf641acf9ad
Richard Wagner <richard.wagner.sd@gmail.com>
2025-07-30 06:26:36 +0200

    fix: card

--- src/routes/core/card/Page.tsx
+++ src/routes/core/card/Page.tsx
@@ -1,25 +1,11 @@
 import type { ReactNode } from 'react';
 import { ExampleTitle } from '../../../components/ExampleTitle';
-import { ColorExample } from './ColorExample';
 import { DefaultExample } from './DefaultExample';
-import { GapExample } from './GapExample';
-import { OrientationExample } from './OrientationExample';
-import { PaddingExample } from './PaddingExample';
-import { RadiusExample } from './RadiusExample';
-import { ShadowExample } from './ShadowExample';
-import { VariantExample } from './VariantExample';
 
 const Page = (): ReactNode => (
   <>
     <ExampleTitle>Card</ExampleTitle>
     <DefaultExample />
-    <VariantExample />
-    <OrientationExample />
-    <PaddingExample />
-    <RadiusExample />
-    <ColorExample />
-    <ShadowExample />
-    <GapExample />
   </>
 );
 
--- src/routes/core/card/index.tsx
+++ src/routes/core/card/index.tsx
@@ -3,5 +3,9 @@ import type { RouteObject } from 'react-router';
 
 export const CardRoute: RouteObject = {
   path: '/core/card',
-  Component: lazy(() => import('./Page'))
+  Component: lazy(
+    (): Promise<
+      typeof import('/Users/ritchiw/vscode/testing/src/routes/core/card/Page')
+    > => import('./Page')
+  )
 };
