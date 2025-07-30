401704bc714a72d836f3e812af6e43332bd5d44b
Richard Wagner <richard.wagner.sd@gmail.com>
2025-07-30 06:56:58 +0200

    fix: circular

--- src/routes/core/circular-progress/DefaultExample.tsx
+++ src/routes/core/circular-progress/DefaultExample.tsx
@@ -7,6 +7,7 @@ export const DefaultExample = (): ReactNode => (
   <ExampleSection title="Default">
     <ExampleCard>
       <Example />
+      <Example value={50}>50%</Example>
     </ExampleCard>
   </ExampleSection>
 );
