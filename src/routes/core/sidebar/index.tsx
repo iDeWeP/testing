a62dda11eea9e3f214c59d2955eb34c08978eb96
Richard Wagner <richard.wagner.sd@gmail.com>
2025-07-30 09:54:08 +0200

    fix: sidebar

--- src/routes/core/sidebar/index.tsx
+++ src/routes/core/sidebar/index.tsx
@@ -3,5 +3,9 @@ import type { RouteObject } from 'react-router';
 
 export const SidebarRoute: RouteObject = {
   path: '/core/sidebar',
-  Component: lazy(() => import('./Page'))
+  Component: lazy(
+    (): Promise<
+      typeof import('/Users/ritchiw/vscode/testing/src/routes/core/sidebar/Page')
+    > => import('./Page')
+  )
 };
