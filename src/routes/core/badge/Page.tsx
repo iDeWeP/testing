e4f2e29db857552a75d39130d833f7fdbe06ac6a
Richard Wagner <richard.wagner.sd@gmail.com>
2025-07-30 06:07:07 +0200

    fix: badge

--- src/routes/core/badge/Page.tsx
+++ src/routes/core/badge/Page.tsx
@@ -1,25 +1,11 @@
 import type { ReactNode } from 'react';
 import { ExampleTitle } from '../../../components/ExampleTitle';
-import { ColorExample } from './ColorExample';
 import { DefaultExample } from './DefaultExample';
-import { MaxExample } from './MaxExample';
-import { OverlapExample } from './OverlapExample';
-import { PlacementExample } from './PlacementExample';
-import { ShowZeroExample } from './ShowZeroExample';
-import { SizeExample } from './SizeExample';
-import { VariantExample } from './VariantExample';
 
 const Page = (): ReactNode => (
   <>
     <ExampleTitle>Badge</ExampleTitle>
     <DefaultExample />
-    <MaxExample />
-    <ShowZeroExample />
-    <VariantExample />
-    <PlacementExample />
-    <OverlapExample />
-    <SizeExample />
-    <ColorExample />
   </>
 );
 
