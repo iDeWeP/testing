b681ade0231bac3a44fde7dc0c70767df7b67899
Richard Wagner <richard.wagner.sd@gmail.com>
2025-07-30 10:57:03 +0200

    fix:

--- src/routes/core/Transition/DefaultExample.tsx
+++ src/routes/core/Transition/DefaultExample.tsx
@@ -1,12 +1,9 @@
 import type { ReactNode } from 'react';
-import { ExampleCard } from '../../../components/ExampleCard';
 import { ExampleSection } from '../../../components/ExampleSection';
 import { Example } from './Example';
 
 export const DefaultExample = (): ReactNode => (
   <ExampleSection title="Default">
-    <ExampleCard>
-      <Example />
-    </ExampleCard>
+    <Example />
   </ExampleSection>
 );
