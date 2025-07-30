07ce61c4168aa09570f4d8b7715df2e7b386f477
Richard Wagner <richard.wagner.sd@gmail.com>
2025-07-30 10:51:41 +0200

    fix ;title

--- src/routes/core/title/index.tsx
+++ src/routes/core/title/index.tsx
@@ -3,5 +3,9 @@ import type { RouteObject } from 'react-router';
 
 export const TitleRoute: RouteObject = {
   path: '/core/title',
-  Component: lazy(() => import('./Page'))
+  Component: lazy(
+    (): Promise<
+      typeof import('/Users/ritchiw/vscode/testing/src/routes/core/title/Page')
+    > => import('./Page')
+  )
 };
