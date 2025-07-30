4670ed2f641079790406359f832c00e25aff5925
Richard Wagner <richard.wagner.sd@gmail.com>
2025-07-30 10:04:48 +0200

    fix; text

--- src/routes/core/text/index.tsx
+++ src/routes/core/text/index.tsx
@@ -3,5 +3,9 @@ import type { RouteObject } from 'react-router';
 
 export const TextRoute: RouteObject = {
   path: '/core/text',
-  Component: lazy(() => import('./Page'))
+  Component: lazy(
+    (): Promise<
+      typeof import('/Users/ritchiw/vscode/testing/src/routes/core/text/Page')
+    > => import('./Page')
+  )
 };
