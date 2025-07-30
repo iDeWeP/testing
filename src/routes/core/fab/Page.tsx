df31e2da820ef4f5a6287a7d2ebcee4fb0b8d4f2
Richard Wagner <richard.wagner.sd@gmail.com>
2025-07-30 07:46:53 +0200

    fix: fab

--- src/routes/core/fab/Page.tsx
+++ src/routes/core/fab/Page.tsx
@@ -1,31 +1,11 @@
 import type { ReactNode } from 'react';
 import { ExampleTitle } from '../../../components/ExampleTitle';
-import { ColorExample } from './ColorExample';
 import { DefaultExample } from './DefaultExample';
-import { DisabledExample } from './DisabledExample';
-import { EffectExample } from './EffectExample';
-import { LoadingExample } from './LoadingExample';
-import { PlacementExample } from './PlacementExample';
-import { PositionExample } from './PositionExample';
-import { RadiusExample } from './RadiusExample';
-import { ScaleExample } from './ScaleExample';
-import { SizeExample } from './SizeExample';
-import { VariantExample } from './VariantExample';
 
 const Page = (): ReactNode => (
   <>
     <ExampleTitle>Fab</ExampleTitle>
     <DefaultExample />
-    <LoadingExample />
-    <DisabledExample />
-    <PositionExample />
-    <PlacementExample />
-    <VariantExample />
-    <SizeExample />
-    <ScaleExample />
-    <RadiusExample />
-    <ColorExample />
-    <EffectExample />
   </>
 );
 
--- src/routes/core/fab/index.tsx
+++ src/routes/core/fab/index.tsx
@@ -3,5 +3,9 @@ import type { RouteObject } from 'react-router';
 
 export const FabRoute: RouteObject = {
   path: '/core/fab',
-  Component: lazy(() => import('./Page'))
+  Component: lazy(
+    (): Promise<
+      typeof import('/Users/ritchiw/vscode/testing/src/routes/core/fab/Page')
+    > => import('./Page')
+  )
 };
