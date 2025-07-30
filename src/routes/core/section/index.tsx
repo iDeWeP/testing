f1154918d86e898870a6a8348935d1ae9424caba
Richard Wagner <richard.wagner.sd@gmail.com>
2025-07-30 09:52:06 +0200

    fix: section

--- src/routes/core/section/index.tsx
+++ src/routes/core/section/index.tsx
@@ -3,5 +3,9 @@ import type { RouteObject } from 'react-router';
 
 export const SectionRoute: RouteObject = {
   path: '/core/section',
-  Component: lazy(() => import('./Page'))
+  Component: lazy(
+    (): Promise<
+      typeof import('/Users/ritchiw/vscode/testing/src/routes/core/section/Page')
+    > => import('./Page')
+  )
 };
