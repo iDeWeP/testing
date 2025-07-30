fe450bbed068566527fa8022dbfe0d804b19e938
Richard Wagner <richard.wagner.sd@gmail.com>
2025-07-30 18:27:11 +0200

    fix: list-item

---
+++ src/routes/core/list-item/Page.tsx
@@ -0,0 +1,12 @@
+import type { ReactNode } from 'react';
+import { ExampleTitle } from '../../../components/ExampleTitle';
+import { DefaultExample } from './DefaultExample';
+
+const Page = (): ReactNode => (
+  <>
+    <ExampleTitle>List-item</ExampleTitle>
+    <DefaultExample />
+  </>
+);
+
+export default Page;
--- src/routes/core/list-item/index.ts
+++ src/routes/core/list-item/index.ts
@@ -3,5 +3,9 @@ import type { RouteObject } from 'react-router';
 
 export const ListItemRoute: RouteObject = {
   path: '/core/list-item',
-  Component: lazy(() => import('./Page'))
+  Component: lazy(
+    (): Promise<
+      typeof import('/Users/ritchiw/vscode/testing/src/routes/core/list-item/Page')
+    > => import('./Page')
+  )
 };
