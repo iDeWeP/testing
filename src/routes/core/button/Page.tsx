968b19ca2e966a1552b1d854905817d2f4098532
Richard Wagner <richard.wagner.sd@gmail.com>
2025-07-30 06:20:03 +0200

    fix: button

--- src/routes/core/button/Page.tsx
+++ src/routes/core/button/Page.tsx
@@ -1,27 +1,11 @@
 import type { ReactNode } from 'react';
 import { ExampleTitle } from '../../../components/ExampleTitle';
-import { ColorExample } from './ColorExample';
 import { DefaultExample } from './DefaultExample';
-import { DisabledExample } from './DisabledExample';
-import { EffectExample } from './EffectExample';
-import { LoadingExample } from './LoadingExample';
-import { RadiusExample } from './RadiusExample';
-import { ScaleExample } from './ScaleExample';
-import { SizeExample } from './SizeExample';
-import { VariantExample } from './VariantExample';
 
 const Page = (): ReactNode => (
   <>
     <ExampleTitle>Button</ExampleTitle>
     <DefaultExample />
-    <LoadingExample />
-    <DisabledExample />
-    <VariantExample />
-    <SizeExample />
-    <ScaleExample />
-    <RadiusExample />
-    <ColorExample />
-    <EffectExample />
   </>
 );
 
--- src/routes/core/button/index.tsx
+++ src/routes/core/button/index.tsx
@@ -3,5 +3,9 @@ import type { RouteObject } from 'react-router';
 
 export const ButtonRoute: RouteObject = {
   path: '/core/button',
-  Component: lazy(() => import('./Page'))
+  Component: lazy(
+    (): Promise<
+      typeof import('/Users/ritchiw/vscode/testing/src/routes/core/button/Page')
+    > => import('./Page')
+  )
 };
