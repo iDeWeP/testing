5d2ace2333f569a3edee20f3e63885a16558fc9c
Richard Wagner <richard.wagner.sd@gmail.com>
2025-07-30 06:30:24 +0200

    fix: card-body

--- src/routes/core/card-body/Page.tsx
+++ src/routes/core/card-body/Page.tsx
@@ -1,24 +1,11 @@
-import type { ReactNode } from "react";
-import { ExampleTitle } from "../../../components/ExampleTitle";
-import { ColorExample } from "./ColorExample";
-import { DefaultExample } from "./DefaultExample";
-import { GapExample } from "./GapExample";
-import { OrientationExample } from "./OrientationExample";
-import { PaddingExample } from "./PaddingExample";
-import { PlacementExample } from "./PlacementExample";
-import { RadiusExample } from "./RadiusExample";
-import { VariantExample } from "./VariantExample";
+import type { ReactNode } from 'react';
+import { ExampleTitle } from '../../../components/ExampleTitle';
+import { DefaultExample } from './DefaultExample';
+
 const Page = (): ReactNode => (
   <>
-    <ExampleTitle>CardBody</ExampleTitle>
+    <ExampleTitle>Card-body</ExampleTitle>
     <DefaultExample />
-    <VariantExample />
-    <OrientationExample />
-    <PlacementExample />
-    <PaddingExample />
-    <RadiusExample />
-    <ColorExample />
-    <GapExample />
   </>
 );
 
--- src/routes/core/card-body/index.tsx
+++ src/routes/core/card-body/index.tsx
@@ -3,5 +3,9 @@ import type { RouteObject } from 'react-router';
 
 export const CardBodyRoute: RouteObject = {
   path: '/core/card-body',
-  Component: lazy(() => import('./Page'))
+  Component: lazy(
+    (): Promise<
+      typeof import('/Users/ritchiw/vscode/testing/src/routes/core/card-body/Page')
+    > => import('./Page')
+  )
 };
