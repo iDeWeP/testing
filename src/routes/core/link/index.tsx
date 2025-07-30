2d74df09482a17876664faab4b30f36b10d76a68
Richard Wagner <richard.wagner.sd@gmail.com>
2025-07-30 09:09:33 +0200

    fix: link

--- src/routes/core/link/index.tsx
+++ src/routes/core/link/index.tsx
@@ -3,5 +3,9 @@ import type { RouteObject } from 'react-router';
 
 export const LinkRoute: RouteObject = {
   path: '/core/link',
-  Component: lazy(() => import('./Page'))
+  Component: lazy(
+    (): Promise<
+      typeof import('/Users/ritchiw/vscode/testing/src/routes/core/link/Page')
+    > => import('./Page')
+  )
 };
