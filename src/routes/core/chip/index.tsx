0b9a61405eeed3179d0b3d8e7726e239e4fc6557
Richard Wagner <richard.wagner.sd@gmail.com>
2025-07-30 06:48:49 +0200

    fix: chip

--- src/routes/core/chip/index.tsx
+++ src/routes/core/chip/index.tsx
@@ -3,5 +3,9 @@ import type { RouteObject } from 'react-router';
 
 export const ChipRoute: RouteObject = {
   path: '/core/chip',
-  Component: lazy(() => import('./Page'))
+  Component: lazy(
+    (): Promise<
+      typeof import('/Users/ritchiw/vscode/testing/src/routes/core/chip/Page')
+    > => import('./Page')
+  )
 };
