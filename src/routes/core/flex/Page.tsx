aac90d7bdb10cfbed67c5d38a9a573641ce49b83
Richard Wagner <richard.wagner.sd@gmail.com>
2025-07-30 07:49:30 +0200

    fix: flex

--- src/routes/core/flex/Page.tsx
+++ src/routes/core/flex/Page.tsx
@@ -1,13 +1,11 @@
 import type { ReactNode } from 'react';
-import type { ExampleTitle } from '../../../components/ExampleTitle';
+import { ExampleTitle } from '../../../components/ExampleTitle';
 import { DefaultExample } from './DefaultExample';
-import { GapExample } from './GapExample';
 
 const Page = (): ReactNode => (
   <>
     <ExampleTitle>Flex</ExampleTitle>
     <DefaultExample />
-    <GapExample />
   </>
 );
 
--- src/routes/core/flex/index.tsx
+++ src/routes/core/flex/index.tsx
@@ -3,5 +3,9 @@ import type { RouteObject } from 'react-router';
 
 export const FlexRoute: RouteObject = {
   path: '/core/flex',
-  Component: lazy(() => import('./Page'))
+  Component: lazy(
+    (): Promise<
+      typeof import('/Users/ritchiw/vscode/testing/src/routes/core/flex/Page')
+    > => import('./Page')
+  )
 };
