fbbf0651b5d3822f35ef0dce4816cd89172d752f
Richard Wagner <richard.wagner.sd@gmail.com>
2025-07-30 06:52:37 +0200

    fix: circular-progress

--- src/routes/core/circular-progress/Page.tsx
+++ src/routes/core/circular-progress/Page.tsx
@@ -1,27 +1,11 @@
 import type { ReactNode } from 'react';
 import { ExampleTitle } from '../../../components/ExampleTitle';
-import { ColorExample } from './ColorExample';
 import { DefaultExample } from './DefaultExample';
-import { DisabledExample } from './DisabledExample';
-import { GapExample } from './GapExample';
-import { SizeExample } from './SizeExample';
-import { SpinExample } from './SpinExample';
-import { ThicknessExample } from './ThicknessExample';
-import { ValueExample } from './ValueExample';
-import { VariantExample } from './VariantExample';
 
 const Page = (): ReactNode => (
   <>
-    <ExampleTitle>CircularProgress</ExampleTitle>
+    <ExampleTitle>Circular-progress</ExampleTitle>
     <DefaultExample />
-    <DisabledExample />
-    <SpinExample />
-    <ValueExample />
-    <VariantExample />
-    <SizeExample />
-    <ThicknessExample />
-    <ColorExample />
-    <GapExample />
   </>
 );
 
--- src/routes/core/circular-progress/index.tsx
+++ src/routes/core/circular-progress/index.tsx
@@ -3,5 +3,9 @@ import type { RouteObject } from 'react-router';
 
 export const CircularProgressRoute: RouteObject = {
   path: '/core/circular-progress',
-  Component: lazy(() => import('./Page'))
+  Component: lazy(
+    (): Promise<
+      typeof import('/Users/ritchiw/vscode/testing/src/routes/core/circular-progress/Page')
+    > => import('./Page')
+  )
 };
