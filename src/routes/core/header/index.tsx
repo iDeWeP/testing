22abf6ae4f00eac8cd3619fc97e567172f6949d5
Richard Wagner <richard.wagner.sd@gmail.com>
2025-07-30 08:01:21 +0200

    fix: header

--- src/routes/core/header/index.tsx
+++ src/routes/core/header/index.tsx
@@ -3,5 +3,9 @@ import type { RouteObject } from 'react-router';
 
 export const HeaderRoute: RouteObject = {
   path: '/core/header',
-  Component: lazy(() => import('./Page'))
+  Component: lazy(
+    (): Promise<
+      typeof import('/Users/ritchiw/vscode/testing/src/routes/core/header/Page')
+    > => import('./Page')
+  )
 };
