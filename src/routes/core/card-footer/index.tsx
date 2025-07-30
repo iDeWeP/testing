0d29e3acdde3776b77916579fa5e85a6f0f6b7a4
Richard Wagner <richard.wagner.sd@gmail.com>
2025-07-30 06:34:12 +0200

    fix: card-footer

--- src/routes/core/card-footer/index.tsx
+++ src/routes/core/card-footer/index.tsx
@@ -3,5 +3,9 @@ import type { RouteObject } from 'react-router';
 
 export const CardFooterRoute: RouteObject = {
   path: '/core/card-footer',
-  Component: lazy(() => import('./Page'))
+  Component: lazy(
+    (): Promise<
+      typeof import('/Users/ritchiw/vscode/testing/src/routes/core/card-footer/Page')
+    > => import('./Page')
+  )
 };
